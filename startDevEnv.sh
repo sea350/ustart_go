98c8d3ed49cde994b25769bfe376abe8d5545e27
Adam <as8513@nyu.edu>
2017-07-12 20:20:33 -0400

    Docker ye
    ye

--- startDevEnv.sh
+++ startDevEnv.sh
@@ -4,4 +4,5 @@ echo "Whenever you want to compile/run/test code, do it from this terminal."
 echo "If you save code in the directory where this script lives, it will also be saved in the docker container, at /home/dev"
 echo "If you stop the container, your saved code will be retained."
 echo "Typing 'exit' will terminate the container's runtime, and return you to your normal terminal."
+docker build -t ustart_go_dev ./
 docker run -v $DEV_DIR:/home/dev -it ustart_go_dev /bin/sh
