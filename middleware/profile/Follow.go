57b39dd4a6c28eca6f3aed2ee97f6b302068d9a9
Steven Armanios <sea350@nyu.edu>
2018-01-11 13:02:40 -0500

    NIL UR DEBUG TEXT IS GARBAGE haha lol jk haha lol not jk fix this

--- middleware/profile/Follow.go
+++ middleware/profile/Follow.go
@@ -21,16 +21,19 @@ func Follow(w http.ResponseWriter, r *http.Request) {
 
 	isFollowed, err4 := uses.IsFollowed(eclient, session.Values["DocID"].(string), fname)
 	if err4 != nil {
+		fmt.Println("this is an error (Follow.go: 24)")
 		fmt.Println(err4)
 	}
 	if isFollowed == true {
 		err := uses.UserUnfollow(eclient, session.Values["DocID"].(string), fname)
 		if err != nil {
+			fmt.Println("this is an error (Follow.go: 30)")
 			fmt.Println(err)
 		}
 	} else {
 		err := uses.UserFollow(eclient, session.Values["DocID"].(string), fname)
 		if err != nil {
+			fmt.Println("this is an error (Follow.go: 36)")
 			fmt.Println(err)
 		}
 	}
