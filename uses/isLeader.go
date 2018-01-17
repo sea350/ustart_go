231b015ad5e0dc92851ae0b62b5569667f68a580
mrb721 <mrb_2059@yahoo.com>
2018-01-17 12:36:46 -0500

    errors

--- uses/isLeader.go
+++ uses/isLeader.go
@@ -1,15 +1,13 @@
 package uses
 
 import (
-	getProject "github.com/sea350/ustart_go/get/project"
 	elastic "gopkg.in/olivere/elastic.v5"
 )
-
 
 //IsLeader ...
 //Returns bool to represent whether member is project leader
 //ALso returns index of member
-func IsLeader(eclient *elastic.Client, memberID) (bool, int) {
+func IsLeader(eclient *elastic.Client, memberID string) (bool, int) {
 	var idx int
 	for i := range proj.Members {
 		idx = i
