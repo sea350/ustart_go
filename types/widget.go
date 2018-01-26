4ba46ae2c3b8147da10aa7042a3271365ea3d365
Steven Armanios <sea350@nyu.edu>
2018-01-26 13:29:57 -0500

    new facy widget struct

--- types/widget.go
+++ types/widget.go
@@ -1,9 +1,13 @@
 package types
 
 type Widget struct {
-	UserID         string `json:UserID`
-	Title          string `json:Title`
-	Description    string `json:Description`
-	Position       int    `json:Position`
-	Classification int    `json:Classification`
+	UserID         string   `json:UserID`
+	Data           []string `json:Title`
+	Position       int      `json:Position`
+	Classification int      `json:Classification`
+	//PLS add data configuration for each classification
+	//class 0 = text widget
+	//	Data[0] = title
+	// 	Data[1] = description
+
 }
