4ae5933e7fc884a09f96ee92118dc4c0720fc37d
Steven Armanios <sea350@nyu.edu>
2018-01-26 16:45:13 -0500

    wat?

--- middleware/profile/addWidget.go
+++ middleware/profile/addWidget.go
@@ -27,9 +27,10 @@ func AddWidget(w http.ResponseWriter, r *http.Request) {
 		//position == len(user.widgets)
 		newWidget := types.Widget{UserID: session.Values["DocID"].(string), Data: data, Position: 0, Classification: 0}
 		err := uses.AddWidget(eclient, session.Values["DocID"].(string), newWidget)
-		fmt.Println(err)
-		fmt.Println("this is an error: middleware/profile/addWidget.go 34")
-		fmt.Println("this is debug text: middleware/profile/addWidget.go 32, add widget executed")
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("this is an error: middleware/profile/addWidget.go 34")
+		}
 	}
 
 	//contentArray := []rune(comment)
