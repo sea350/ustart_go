a1c1a5c1fd3391ad5752457dfdb3e7102d7492fb
Steven Armanios <sea350@nyu.edu>
2018-02-02 16:22:33 -0500

    wwwwwwwwwwwwwwwwww

--- middleware/profile/addWidget.go
+++ middleware/profile/addWidget.go
@@ -124,6 +124,8 @@ func AddWidget(w http.ResponseWriter, r *http.Request) {
 
 	if r.FormValue("editID") == `0` {
 		err := uses.AddWidget(eclient, session.Values["DocID"].(string), newWidget)
+		fmt.Println("Widget added. Classification:")
+		fmt.Println(newWidget.Classification)
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println("this is an error: middleware/profile/addWidget.go 128")
