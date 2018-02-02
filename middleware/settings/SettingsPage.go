eb6814035112720735e2ebf470261686d71be8c3
Steven Armanios <sea350@nyu.edu>
2018-02-02 12:55:38 -0500

    semantic

--- middleware/settings/SettingsPage.go
+++ middleware/settings/SettingsPage.go
@@ -1,31 +1,29 @@
-package settings 
+package settings
 
 import (
-    "net/http"
-    uses "github.com/sea350/ustart_go/uses"
-    elastic "gopkg.in/olivere/elastic.v5"
-    "fmt"
-    "github.com/gorilla/sessions"
-    client "github.com/sea350/ustart_go/middleware/clientstruct"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+	client "github.com/sea350/ustart_go/middleware/client"
+	uses "github.com/sea350/ustart_go/uses"
+	elastic "gopkg.in/olivere/elastic.v5"
 )
 
 var eclient, err = elastic.NewClient(elastic.SetURL("http://localhost:9200"))
-var store = sessions.NewCookieStore([]byte("RIU3389D1")) // code 
+var store = sessions.NewCookieStore([]byte("RIU3389D1")) // code
 
-
-func Settings(w http.ResponseWriter, r *http.Request){
+//Settings ...
+func Settings(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session_please")
 	test1, _ := session.Values["DocID"]
-     if (test1 == nil){
-     	fmt.Println(test1)
-     http.Redirect(w, r, "/~", http.StatusFound)
-       }
-    userstruct, _,_,_ := uses.UserPage(eclient,session.Values["Username"].(string),session.Values["DocID"].(string))
-    cs := client.ClientSide{UserInfo:userstruct, DOCID:session.Values["DocID"].(string)}  
-	client.RenderTemplate(w,"template2-nil",cs)
-	client.RenderTemplate(w,"settings-Nil",cs)
-		
-
+	if test1 == nil {
+		fmt.Println(test1)
+		http.Redirect(w, r, "/~", http.StatusFound)
+	}
+	userstruct, _, _, _ := uses.UserPage(eclient, session.Values["Username"].(string), session.Values["DocID"].(string))
+	cs := client.ClientSide{UserInfo: userstruct, DOCID: session.Values["DocID"].(string)}
+	client.RenderTemplate(w, "template2-nil", cs)
+	client.RenderTemplate(w, "settings-Nil", cs)
 
 }
-
