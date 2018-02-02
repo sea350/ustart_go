d0ed32a63753937b238219323466937c2a5d39c3
mrb721 <mrb_2059@yahoo.com>
2018-02-02 14:02:44 -0500

    reindex/update widget

--- post/widget/updateWidget.go
+++ post/widget/updateWidget.go
@@ -38,3 +38,33 @@ func UpdateWidget(eclient *elastic.Client, widgetID string, field string, newCon
 
 	return err
 }
+
+// //UpdateWidget ...
+// //  Change a single field of the ES Document
+// //  Return an error, nil if successful
+// func UpdateWidgetData(eclient *elastic.Client, widgetID string, index int) error {
+
+// ctx := context.Background()
+
+// exists, err := eclient.IndexExists(globals.WidgetIndex).Do(ctx)
+// if err != nil {
+// 	return err
+// }
+// if !exists {
+// 	return errors.New("Index does not exist")
+// }
+
+// _, err = get.WidgetByID(eclient, widgetID)
+// if err != nil {
+// 	return err
+// }
+
+// _, err = eclient.Update().
+// 	Index(globals.WidgetIndex).
+// 	Type(globals.WidgetType).
+// 	Id(widgetID).
+// 	Doc(map[string]interface{}{field: newContent}).
+// 	Do(ctx)
+
+// return err
+// }
