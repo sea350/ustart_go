896b9f03fde9d169f6ead3c4f68f40268156b71c
Steven Armanios <sea350@nyu.edu>
2018-02-16 16:14:47 -0500

    new template directories

--- middleware/client/client.go
+++ middleware/client/client.go
@@ -39,18 +39,18 @@ type ClientSide struct {
 }
 
 /* The following line is how HTML is loaded by our application. Note we need the relative link from the location of GoStart2. */
-var templates = htype.Must(htype.ParseFiles("/home/rr2396/www/ustart.tech/followerlist-nil.html",
-	"/home/rr2396/www/ustart.tech/emTee.html", "/home/rr2396/www/ustart.tech/wallttt.html",
-	"/home/rr2396/www/ustart.tech/wallload-nil.html", "/home/rr2396/www/ustart.tech/testimage.html",
-	"/home/rr2396/www/ustart.tech/ajax-nil.html", "/home/rr2396/www/ustart.tech/Membership-Nil.html",
-	"/home/rr2396/www/ustart.tech/settings-Nil.html", "/home/rr2396/www/ustart.tech/inbox-Nil.html",
-	"/home/rr2396/www/ustart.tech/createProject-Nil.html", "/home/rr2396/www/ustart.tech/manageprojects-Nil.html",
-	"/home/rr2396/www/ustart.tech/projectsF.html", "/home/rr2396/www/ustart.tech/new-reg-nil.html",
-	"/home/rr2396/www/ustart.tech/loginerror-nil.html", "/home/rr2396/www/ustart.tech/test.html",
-	"/home/rr2396/www/ustart.tech/payment-nil.html", "/home/rr2396/www/ustart.tech/templateNoUser2.html",
-	"/home/rr2396/www/ustart.tech/profile-nil.html", "/home/rr2396/www/ustart.tech/template2-nil.html",
-	"/home/rr2396/www/ustart.tech/template-footer-nil.html", "/home/rr2396/www/ustart.tech/nil-index2.html",
-	"/home/rr2396/www/ustart.tech/regcomplete-nil.html"))
+var templates = htype.Must(htype.ParseFiles("/ustart/ustart_front/followerlist-nil.html",
+	"/ustart/ustart_front/emTee.html", "/ustart/ustart_front/wallttt.html",
+	"/ustart/ustart_front/wallload-nil.html", "/ustart/ustart_front/testimage.html",
+	"/ustart/ustart_front/ajax-nil.html", "/ustart/ustart_front/Membership-Nil.html",
+	"/ustart/ustart_front/settings-Nil.html", "/ustart/ustart_front/inbox-Nil.html",
+	"/ustart/ustart_front/createProject-Nil.html", "/ustart/ustart_front/manageprojects-Nil.html",
+	"/ustart/ustart_front/projectsF.html", "/ustart/ustart_front/new-reg-nil.html",
+	"/ustart/ustart_front/loginerror-nil.html", "/ustart/ustart_front/test.html",
+	"/ustart/ustart_front/payment-nil.html", "/ustart/ustart_front/templateNoUser2.html",
+	"/ustart/ustart_front/profile-nil.html", "/ustart/ustart_front/template2-nil.html",
+	"/ustart/ustart_front/template-footer-nil.html", "/ustart/ustart_front/nil-index2.html",
+	"/ustart/ustart_front/regcomplete-nil.html"))
 
 //RenderTemplate ... This function does the actual rendering of HTML pages. Note it takes in a struct (type ClientSide).
 //You will need to continually send data to the pages and this is accomplished via the struct.
