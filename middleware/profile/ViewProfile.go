25ff41808a793cff5ee3d77b8af53380be7032d2
sea350 <sea350@nyu.edu>
2018-02-23 14:20:06 -0500

    debug text

--- middleware/profile/ViewProfile.go
+++ middleware/profile/ViewProfile.go
@@ -19,13 +19,15 @@ var store = sessions.NewCookieStore([]byte("RIU3389D1")) // code
 func ViewProfile(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session_please")
 	test1, _ := session.Values["DocID"]
+	fmt.Println("this is  test1 = session.Values[DocID]")
+	fmt.Println(test1)
 	if test1 == nil {
 		http.Redirect(w, r, "/~", http.StatusFound)
 	}
 
-	userstruct, _, followbool, err5 := uses.UserPage(eclient, r.URL.Path[9:], session.Values["DocID"].(string))
-	fmt.Println("ViewProfile line 28 will print a DocID:")
-	fmt.Println(session.Values["DocID"].(string))
+	userstruct, _, followbool, err5 := uses.UserPage(eclient, r.URL.Path[9:], test1.(string))
+	fmt.Println("ViewProfile line 28 will print session.Values[Username].(string):")
+	fmt.Println(session.Values["Username"].(string))
 	if err5 != nil {
 		fmt.Println("this is an error (ViewProfile.go: 29)")
 		fmt.Println(err5)
