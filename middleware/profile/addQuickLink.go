ab471ed16fdb478464d700c1969fdb319951163a
Steven Armanios <sea350@nyu.edu>
2018-02-23 12:38:51 -0500

    fprint

--- middleware/profile/addQuickLink.go
+++ middleware/profile/addQuickLink.go
@@ -34,4 +34,7 @@ func AddQuickLink(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("this is an err: middleware/profile/addQuickLink line 31")
 	}
 
+	fmt.Fprint(w, r.FormValue("userLinkDesc"))
+	fmt.Fprint(w, r.FormValue("userLink"))
+
 }
