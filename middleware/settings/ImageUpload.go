30d4d4270ad0875f216fd8b7f41d523e0ce9f6af
Steven Armanios <sea350@nyu.edu>
2018-02-23 12:04:54 -0500

    image upload no chops off first char, sign up basic has a default img

--- middleware/settings/ImageUpload.go
+++ middleware/settings/ImageUpload.go
@@ -1,30 +1,28 @@
-package settings 
+package settings
 
 import (
-    "net/http"
-    uses "github.com/sea350/ustart_go/uses"
-    "fmt"
+	"fmt"
+	"net/http"
+
+	uses "github.com/sea350/ustart_go/uses"
 )
 
-func ImageUpload(w http.ResponseWriter, r *http.Request){
+//ImageUpload ...
+func ImageUpload(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session_please")
 	test1, _ := session.Values["DocID"]
-    if (test1 == nil){
-     	fmt.Println(test1)
-     	http.Redirect(w, r, "/~", http.StatusFound)
-    }
-    r.ParseForm()
-    blob := r.FormValue("image-data")
-    blob = blob[1:len(blob)]
- //   fmt.Println(blob)
+	if test1 == nil {
+		fmt.Println(test1)
+		http.Redirect(w, r, "/~", http.StatusFound)
+	}
+	r.ParseForm()
+	blob := r.FormValue("image-data")
 
-      //fmt.Println(reflect.TypeOf(blob))
-    err := uses.ChangeAccountImagesAndStatus(eclient, session.Values["DocID"].(string),blob,true,"hello","Avatar");
-    if (err != nil){
-    	fmt.Println(err);
-    }
+	err := uses.ChangeAccountImagesAndStatus(eclient, session.Values["DocID"].(string), blob, true, "hello", "Avatar")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    http.Redirect(w, r, "/profile/"+session.Values["Username"].(string), http.StatusFound)
+	http.Redirect(w, r, "/profile/"+session.Values["Username"].(string), http.StatusFound)
 
 }
-
