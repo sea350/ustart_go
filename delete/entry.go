13c0ec9fa8be3fd34206ad44841899f9134b48ac
mrb721 <mrb_2059@yahoo.com>
2018-03-02 13:44:34 -0500

    delete entry

---
+++ delete/entry.go
@@ -0,0 +1,28 @@
+package delete
+
+import (
+	elastic "gopkg.in/olivere/elastic.v5"
+	//"errors"
+
+	"context"
+
+	globals "github.com/sea350/ustart_go/globals"
+	//"golang.org/x/crypto/bcrypt"
+)
+
+//Entry ...
+//
+func Entry(eclient *elastic.Client, entryID string) error {
+
+	ctx := context.Background()
+
+	//delete the widget from ES
+	_, err := eclient.Delete().
+		Index(globals.EntryIndex).
+		Type(globals.EntryType).
+		Id(entryID).
+		Do(ctx)
+
+	return err
+
+}
