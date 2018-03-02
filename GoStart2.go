7cf7a894f66722c0417f3fc5ed366b026b339e0b
sea350 <sea350@nyu.edu>
2018-03-02 14:51:49 -0500

    banner upload made

--- GoStart2.go
+++ GoStart2.go
@@ -61,6 +61,7 @@ func main() {
 	http.HandleFunc("/changeLoc/", settings.ChangeLocation)
 	http.HandleFunc("/changeEDU/", settings.ChangeEDU)
 	http.HandleFunc("/UpdateDescription/", settings.ChangeContactAndDescription)
+	http.HandleFunc("/BannerUpload/", settings.BannerUpload)
 
 	http.HandleFunc("/Signup/", registration.Signup)
 	http.HandleFunc("/Registration/Type/", registration.RegisterType)
