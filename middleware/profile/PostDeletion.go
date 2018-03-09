34761e6887287a8e133b54f66576f15f1f8ab986
Nil Patel <np1310@nyu.edu>
2018-03-09 16:35:26 -0500

    delete modal changes

--- middleware/profile/PostDeletion.go
+++ middleware/profile/PostDeletion.go
@@ -20,3 +20,33 @@ func DeleteWallPost(w http.ResponseWriter, r *http.Request){
 	//err := uses.RemoveEntry(eclient,session.Values["DocID"].(string),)
 
 }
+
+func GenerateDeleteModal(w http.ResponseWriter, r *http.Request){
+	session, _ := store.Get(r, "session_please")
+	test1, _ := session.Values["DocID"]
+	if test1 == nil {
+		fmt.Println(test1)
+		http.Redirect(w, r, "/~", http.StatusFound)
+	}
+
+	output := `                            <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<span style="font-size:20px;">Confirm Deletion</span>
+			</div>
+			<div class="modal-body">
+				<span style="font-size:15px;">Are you sure you want to delete this post?</span>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				<form action ="http://ustart.today:5000/deletePost" method ="POST"> 
+<!--                                                 <input type="text" name="postid" value=(this).attr("id")>  
+-->                                            <button type="submit"><a class="btn btn-danger btn-ok">Delete</a></button>
+			</form>
+			</div>
+		</div>
+	</div>
+</div>`
+fmt.Fprintln(w, output)
+}
