fc9cca5ba8a67edecf5babe9d890b816e6a3bec9
Tarek <thassoun000@gmail.com>
2018-03-13 14:09:49 -0400

    Comments for stringParser

--- uses/stringParser.go
+++ uses/stringParser.go
@@ -6,10 +6,18 @@ import (
 
 //StringChecker ...
 func StringChecker(entryCode string, website string) bool {
+	//A lot of these changes are based on this document
+	//https://docs.google.com/document/d/1H6AG11pxFkTyxSsXM8ZjSRRsTlTE4Xb5ZsCqzvNSU-g/edit
+
+	//The Entry Code could be an embed code, a url, or anything else that's related to our website
+
+	//Checks if the website is in our EntryCode
 	ifValidWebsite := strings.Contains(entryCode, website)
 	if !ifValidWebsite {
 		return false
 	}
+
+	//3 following Embed checkers if our EntryCode is an embed code
 	primaryChecker1 := "<iframe"
 	primaryChecker2 := "<div class"
 	primaryChecker3 := "<p data-height"
@@ -41,12 +49,18 @@ func StringChecker(entryCode string, website string) bool {
 		}
 	}
 
+	//If EntryCode is a url and has "http" or "https", check for the "www" keyword
 	if strings.HasPrefix(entryCode, "http") || strings.HasPrefix(entryCode, "https") { //Regular Chekcer
 		if strings.Contains(entryCode, "www."+website) == false {
 			return false
 		}
 	}
 
+	//The following are based on the specifications made from the google doc link listed above
+	//If the EntryCodes are URLs, they will have specific structures based on their website
+	//Pinterest has a /pin/<code> where <code> can be of legnth 18, 55, or 87
+	//Instagram has /p/<code> where <code> is of length 10
+	//Youtube has youtube.com/<keyword> where the keyword is "watch?v=<code>" where <code> is a combination of nums,letters,symbols
 	if website == "pinterest.com" {
 		blockPoint := strings.Split(entryCode, "/")
 		if !(len(blockPoint[4]) == 18 || len(blockPoint[4]) == 55 || len(blockPoint) == 87) {
