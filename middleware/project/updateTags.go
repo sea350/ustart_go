ed3d9d0beb899c79a65e54d6cc306461bd2017ba
sea350 <sea350@nyu.edu>
2018-03-15 17:39:15 -0400

    wat

--- middleware/project/updateTags.go
+++ middleware/project/updateTags.go
@@ -19,7 +19,7 @@ func UpdateTags(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ID := r.FormValue("projectWidget")
-	fmt.Println(session.Values["Project"].(string))
+	fmt.Println(session.Values)
 	fmt.Println("/middleware/project/updatetags.go attempting to print out full session project data struct")
 
 	tags := strings.Split(r.FormValue("skillArray"), `","`)
