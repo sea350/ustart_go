28707ad187781ccf0bf0ad300c0bdb21e5ccd2a1
sea350 <sea350@nyu.edu>
2018-03-16 14:57:35 -0400

    debug text added

--- middleware/project/addQuickLink.go
+++ middleware/project/addQuickLink.go
@@ -19,19 +19,20 @@ func AddQuickLink(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/~", http.StatusFound)
 	}
 	ID := r.FormValue("projectID")
-
+	fmt.Println("debug text. middleware/project/addquicklink. projectID is:")
+	fmt.Println(r.FormValue("projectID"))
 	proj, err := get.ProjectByID(client.Eclient, ID)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("this is an err: middleware/project/addQuickLink line 25")
 	}
-
+	fmt.Println("debug text. middleware/project/addquicklink. line 29")
 	proj.QuickLinks = append(proj.QuickLinks, types.Link{Name: r.FormValue("projectLinkDesc"), URL: r.FormValue("projectLink")})
 
 	err = post.UpdateProject(client.Eclient, ID, "QuickLinks", proj.QuickLinks)
 	if err != nil {
 		fmt.Println(err)
-		fmt.Println("this is an err: middleware/project/addQuickLink line 31")
+		fmt.Println("this is an err: middleware/project/addQuickLink line 33")
 	}
 
 }
