4519da3db1bc1c2e264b7c0ea40cecd083b1153f
mrb721 <mrb_2059@yahoo.com>
2018-03-24 13:51:10 -0400

    project requests

--- post/user/appendSentProjReq.go
+++ post/user/appendSentProjReq.go
@@ -1,20 +1,15 @@
 package post
 
 import (
-	"context"
 	"errors"
 
 	get "github.com/sea350/ustart_go/get/user"
-	globals "github.com/sea350/ustart_go/globals"
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
-//AppendProjReq ... appends to either sent or received project request arrays within user
-//takes in eclient, user ID, the project ID, and a bool
-//true = append to following, false = append to followers
-func AppendProjReq(eclient *elastic.Client, usrID string, projID string, whichOne bool) error {
-	ctx := context.Background()
-
+//AppendSentProjReq ... appends to either sent project request arrays within user
+//takes in eclient, user ID, the project ID
+func AppendSentProjReq(eclient *elastic.Client, usrID string, projID string) error {
 	ProjectLock.Lock()
 
 	usr, err := get.UserByID(eclient, usrID)
@@ -25,12 +20,7 @@ func AppendProjReq(eclient *elastic.Client, usrID string, projID string, whichOn
 
 	usr.SentProjReq = append(usr.SentProjReq, projID)
 
-	_, err = eclient.Update().
-		Index(globals.UserIndex).
-		Type(globals.UserType).
-		Id(usrID).
-		Doc(map[string]interface{}{"SentProjReq": usr.SentProjReq}).
-		Do(ctx)
+	err = UpdateUser(eclient, usrID, "SentProjReq", usr.SentProjReq)
 
 	defer ProjectLock.Unlock()
 	return err
