21b2f93ed374570113f54d48b5dd86c64688ba08
mrb721 <mrb_2059@yahoo.com>
2018-03-24 16:25:51 -0400

    restartDB

---
+++ restartDB.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	globals "github.com/sea350/ustart_go/globals"
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+var eclient, _ = elastic.NewSimpleClient(elastic.SetURL("http://localhost:9200"))
+
+func main() {
+
+	ctx := context.Background()
+	deleteIndex, err := eclient.DeleteIndex(globals.ChatIndex).Do(ctx)
+	deleteIndex, err = eclient.DeleteIndex(globals.EntryIndex).Do(ctx)
+	deleteIndex, err = eclient.DeleteIndex(globals.ProjectIndex).Do(ctx)
+	deleteIndex, err = eclient.DeleteIndex(globals.WidgetIndex).Do(ctx)
+	deleteIndex, err = eclient.DeleteIndex(globals.UserIndex).Do(ctx)
+	if err != nil {
+		// Handle error
+		fmt.Println(err)
+	} else {
+		fmt.Println("S U C C")
+	}
+	if !deleteIndex.Acknowledged {
+		// Not acknowledged
+	}
+
+	ctx := context.Background()
+	createIndex, err := eclient.CreateIndex(globals.UserIndex).BodyString(usrMapping).Do(ctx)
+	createIndex, err = eclient.CreateIndex(globals.ProjectIndex).BodyString(projMapping).Do(ctx)
+	createIndex, err = eclient.CreateIndex(globals.WidgetIndex).BodyString(widgetMapping).Do(ctx)
+	createIndex, err = eclient.CreateIndex(globals.EntryIndex).Do(ctx)
+	if err != nil {
+		// Handle error
+		fmt.Println(err)
+		fmt.Println("Could not create")
+	}
+
+	if !createIndex.Acknowledged {
+		// Not acknowledged
+	}
+
+}
