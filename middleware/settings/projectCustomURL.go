201e699898832062464c4b923a82f93defc8afe1
sea350 <sea350@nyu.edu>
2018-04-07 13:30:45 -0400

    now check for url in use

--- middleware/settings/projectCustomURL.go
+++ middleware/settings/projectCustomURL.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/sea350/ustart_go/middleware/client"
+
 	get "github.com/sea350/ustart_go/get/project"
 	uses "github.com/sea350/ustart_go/uses"
 )
@@ -21,13 +23,27 @@ func ProjectCustomURL(w http.ResponseWriter, r *http.Request) {
 	newURL := r.FormValue("purl")
 
 	projID := r.FormValue("projectID")
-	proj, err := get.ProjectByID(eclient, projID)
+
+	inUse, err := get.URLInUse(client.Eclient, newURL)
 	if err != nil {
+		fmt.Println("err: middleware/settings/projectcustomurl line 28")
 		fmt.Println(err)
+	}
+
+	proj, err := get.ProjectByID(client.Eclient, projID)
+	if err != nil {
+		fmt.Println("err: middleware/settings/projectcustomurl line 34")
+		fmt.Println(err)
+	}
+
+	if inUse {
+		fmt.Println("URL IS IN USE, ERROR NOT PROPERLY HANDLED REDIRECTING TO PROJECT PAGE")
+		http.Redirect(w, r, "/Projects/"+proj.URLName, http.StatusFound)
 	}
 
 	err = uses.ChangeProjectURL(eclient, projID, newURL)
 	if err != nil {
+		fmt.Println("err: middleware/settings/projectcustomurl line 46")
 		fmt.Println(err)
 	}
 
