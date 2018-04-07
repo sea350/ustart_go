645a6e34af004dd466cd58d587f066e12690c1c0
mrb721 <mrb_2059@yahoo.com>
2018-04-07 16:35:25 -0400

    fix

--- app_middleware/login/handler.go
+++ app_middleware/login/handler.go
@@ -20,25 +20,20 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	resp := setupResp()
 
-	if acrh, ok := r.Header["Access-Control-Request-Headers:"]; ok {
+	if acrh, ok := r.Header["Access-Control-Request-Headers"]; ok {
 		w.Header().Set("Access-Control-Allow-Headers", acrh[0])
-
 	}
-
 	w.Header().Set("Access-Control-Allow-Credentials", "True")
 	if acao, ok := r.Header["Access-Control-Allow-Origin"]; ok {
 		w.Header().Set("Access-Control-Allow-Origin", acao[0])
 	} else {
 		if _, oko := r.Header["Origin"]; oko {
 			w.Header().Set("Access-Control-Allow-Origin", r.Header["Origin"][0])
-
 		} else {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
-
 		}
 	}
-
-	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 	w.Header().Set("Connection", "Close")
 
 	//defer json.NewEncoder(w).Encode(resp)
