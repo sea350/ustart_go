37357606d0473c2b1bfbc853c0e6f3da2114a0c8
sea350 <sea350@nyu.edu>
2018-04-13 13:05:20 -0400

    test wall page load first 5 comments

--- middleware/profile/testWallPage.go
+++ middleware/profile/testWallPage.go
@@ -1,8 +1,12 @@
 package profile
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/sea350/ustart_go/uses"
+
+	get "github.com/sea350/ustart_go/get/user"
 	client "github.com/sea350/ustart_go/middleware/client"
 )
 
@@ -15,7 +19,24 @@ func TestWallPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cs := client.ClientSide{}
+	wallUsername := r.URL.Path[10:]
+	wallID, err := get.IDByUsername(client.Eclient, wallUsername)
+	if err != nil {
+		fmt.Println("err middleware/profile/testwallpage line 22")
+		fmt.Println(err)
+	}
+	user, err := get.UserByID(client.Eclient, wallID)
+	if err != nil {
+		fmt.Println("err middleware/profile/testwallpage line 28")
+		fmt.Println(err)
+	}
+	entries, err := uses.LoadEntries(client.Eclient, user.EntryIDs[0:5])
+	if err != nil {
+		fmt.Println("err middleware/profile/testwallpage line 28")
+		fmt.Println(err)
+	}
+
+	cs := client.ClientSide{Page: wallID, Wall: entries}
 
 	client.RenderSidebar(w, r, "template2-nil")
 	client.RenderSidebar(w, r, "leftnav-nil")
