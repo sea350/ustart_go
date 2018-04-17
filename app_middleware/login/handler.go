7ba9ac3997bde5f33dbb3b22abbecfa50f078d33
mrb721 <mrb_2059@yahoo.com>
2018-04-17 14:01:30 -0400

    login fix

--- app_middleware/login/handler.go
+++ app_middleware/login/handler.go
@@ -23,7 +23,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Handling a login request")
 	session, _ := store.Get(r, "session_please")
 	test1, _ := session.Values["Username"]
-	if test1 == nil {
+	if test1 != nil {
 		fmt.Println(test1)
 		http.Redirect(w, r, "/~", http.StatusFound)
 		return
