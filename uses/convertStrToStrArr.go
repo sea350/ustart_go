88df2e5dc0d3f3a3e2266ed833bc2a0871241679
sea350 <sea350@nyu.edu>
2018-04-21 13:47:06 -0400

    pls halp

--- uses/convertStrToStrArr.go
+++ uses/convertStrToStrArr.go
@@ -7,12 +7,14 @@ func ConvertStrToStrArr(unbrokenString string) []string {
 	var stringArray []string
 	stringArray = strings.Split(unbrokenString, `","`)
 	if len(stringArray) > 0 {
-		//checks if input string in right format
-		if len(stringArray[0]) > 2 {
-			if stringArray[0][0:1] != `["` || stringArray[len(stringArray)-1][len(stringArray[len(stringArray)-1])-2:len(stringArray[len(stringArray)-1])-1] != `"]` {
-				return []string{}
+		/*
+			//checks if input string in right format
+			if len(stringArray[0]) > 2 {
+				if stringArray[0][0:1] != `["` || stringArray[len(stringArray)-1][len(stringArray[len(stringArray)-1])-2:len(stringArray[len(stringArray)-1])-1] != `"]` {
+					return []string{}
+				}
 			}
-		}
+		*/
 		stringArray[0] = strings.Trim(stringArray[0], `["`)
 		stringArray[len(stringArray)-1] = strings.Trim(stringArray[len(stringArray)-1], `"]`)
 		if stringArray[0] == `` && len(stringArray) == 1 {
