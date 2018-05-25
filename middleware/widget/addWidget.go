036ef138283648f5e61ec72ce2cce29ad523732f
Gitnil <garudyne@gmail.com>
2018-05-25 17:32:35 -0400

    Debug Text Github Widget 2

--- middleware/widget/addWidget.go
+++ middleware/widget/addWidget.go
@@ -255,7 +255,10 @@ func AddWidget(w http.ResponseWriter, r *http.Request) {
 		//github widget username
 
 		username := template.HTML(r.FormValue("username"))
+		fmt.Println("W")
 		fmt.Println(r.FormValue("username"))
+		fmt.Println("D")
+		fmt.Println(username)
 		data = []template.HTML{username}
 		classification = 16
 	}
