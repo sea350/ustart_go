bfd46df706fc07453758097fe0bffc2fcdd348ab
mrb721 <mrb_2059@yahoo.com>
2018-05-29 14:28:53 -0400

    user mapping

--- globals/globals.go
+++ globals/globals.go
@@ -38,22 +38,22 @@ const MappingUsr = `
             "properties":{
                 "Email":{
 					"type":"keyword",
-					"index" : "not_analyzed"
+					"analyzer":"english"
                 },
                 "Username":{
 					"type":"keyword",
-					"index" : "not_analyzed"
+					"analyzer":"english"
                 },
                <!-- "AccCreation":{
                 	"type": date"
 				},-->
 				"FirstName":{
 					"type": "keyword",
-					"index" : "not_analyzed"
+					"analyzer":"english"
 				},
 				"LastName":{
 					"type":"keyword",
-					"index" : "not_analyzed"
+					"analyzer":"english"
 				}
 				<!--"Tags":{
 					"type":"keyword"-->
