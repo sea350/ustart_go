c14fcbfc47799a674979ebec6b744269b1d6a1c1
Jonchow77 <Jonchow77@gmail.com>
2018-06-01 15:15:06 -0400

    Fixed Image Upload

--- middleware/settings/ImageUpload.go
+++ middleware/settings/ImageUpload.go
@@ -35,12 +35,11 @@ func ImageUpload(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println(http.DetectContentType(buffer)[0:5])
 
-	if http.DetectContentType(buffer)[0:5] != "image" {
-		return
-	}
-	err := uses.ChangeAccountImagesAndStatus(eclient, session.Values["DocID"].(string), blob, true, "hello", "Avatar")
-	if err != nil {
-		fmt.Println(err)
+	if http.DetectContentType(buffer)[0:5] == "image" {
+		err := uses.ChangeAccountImagesAndStatus(eclient, session.Values["DocID"].(string), blob, true, "hello", "Avatar")
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
 	http.Redirect(w, r, "/Settings/#avatarcollapse", http.StatusFound)
