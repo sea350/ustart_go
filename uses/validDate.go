19dd5e62dcf3d64c60dab7518f4338eef8ce6b08
mrb721 <mrb_2059@yahoo.com>
2018-06-05 16:14:23 -0400

    date check

--- uses/validDate.go
+++ uses/validDate.go
@@ -39,11 +39,11 @@ func ValidDate(date string) bool {
 
 	// 	re := regexp.MustCompile("(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)")
 
-	rxDate := regexp.MustCompile("((19|20)\\d\\d)/(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])")
-
 	if len(date) < 11 {
 		return false
 	}
+	rxDate := regexp.MustCompile("((19|20)\\d\\d)/(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])")
+
 	month, errMonth := strconv.Atoi(date[5:7])
 	if errMonth != nil {
 		return false
