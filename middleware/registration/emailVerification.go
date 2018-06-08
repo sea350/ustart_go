73fb108a23bf84c2e742e150c83b6023b1c35980
Jonchow77 <Jonchow77@gmail.com>
2018-06-08 16:30:52 -0400

    Updated email verification process

--- middleware/registration/emailVerification.go
+++ middleware/registration/emailVerification.go
@@ -3,10 +3,46 @@ package registration
 import (
 	"fmt"
 	"net/http"
+
+	get "github.com/sea350/ustart_go/get/user"
+	client "github.com/sea350/ustart_go/middleware/client"
+	post "github.com/sea350/ustart_go/post/user"
 )
 
 func EmailVerification(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("email")
-	token := r.FormValue("verifCode")
-	fmt.Println(email + "  " + token)
+	emailToken := r.FormValue("verifCode")
+
+	userID, err := get.UserIDByEmail(client.Eclient, email)
+	if err != nil {
+		fmt.Println("err: middleware/registration/emailVerification line 16")
+		fmt.Println(err)
+		return
+	}
+
+	user, err1 := get.UserByEmail(client.Eclient, email)
+	if err1 != nil {
+		fmt.Println("err: middleware/registration/emailVerification line 22")
+		fmt.Println(err1)
+		return
+	}
+
+	if emailToken == user.AuthenticationCode {
+		err2 := post.UpdateUser(client.Eclient, userID, "FirstLogin", true)
+		if err2 != nil {
+			fmt.Println("err: middleware/registration/emailVerification line 29")
+			fmt.Println(err2)
+			return
+		}
+		err3 := post.UpdateUser(client.Eclient, userID, "AuthenticationCode", nil)
+		if err3 != nil {
+			fmt.Println("err: middleware/registration/emailVerification line 34")
+			fmt.Println(err3)
+			return
+		}
+	} else {
+		fmt.Println("Verification code is wrong")
+		return
+	}
+
 }
