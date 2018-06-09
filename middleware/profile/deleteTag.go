a79bc782e5e1bec61baa11cd374ac78c2cc213d0
mrb721 <mrb_2059@yahoo.com>
2018-06-09 13:14:54 -0400

    delete tag check

--- middleware/profile/deleteTag.go
+++ middleware/profile/deleteTag.go
@@ -29,40 +29,44 @@ func DeleteTag(w http.ResponseWriter, r *http.Request) {
 
 	deleteTag := r.FormValue("UNKNOWN")
 
-	var newArr []string
+	if ID == r.URL.Path[10:] {
+		fmt.Println("URL PATH:", r.URL.Path[10:])
 
-	if len(usr.Tags) == 1 {
-		err := post.UpdateUser(client.Eclient, ID, "Tags", newArr)
+		var newArr []string
+
+		if len(usr.Tags) == 1 {
+			err := post.UpdateUser(client.Eclient, ID, "Tags", newArr)
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("this is an err: middleware/profile/deleteTag line 35")
+			}
+			return
+		}
+
+		target := -1
+		for index, tag := range usr.Tags {
+
+			if tag == deleteTag {
+				target = index
+				fmt.Println(target)
+				break
+			}
+		}
+
+		if target == -1 {
+			fmt.Println("deleted object not found")
+			fmt.Println("this is an err, middleware/profile/deleteTag line 54")
+			newArr = usr.Tags
+		} else if (target + 1) < len(usr.Tags) {
+			newArr = append(usr.Tags[:target], usr.Tags[(target+1):]...)
+		} else {
+			newArr = usr.Tags[:target]
+		}
+
+		err = post.UpdateUser(client.Eclient, ID, "Tags", newArr)
 		if err != nil {
 			fmt.Println(err)
-			fmt.Println("this is an err: middleware/profile/deleteTag line 35")
+			fmt.Println("this is an err: middleware/profile/deleteTag line 31")
 		}
-		return
-	}
-
-	target := -1
-	for index, tag := range usr.Tags {
-
-		if tag == deleteTag {
-			target = index
-			fmt.Println(target)
-			break
-		}
-	}
-
-	if target == -1 {
-		fmt.Println("deleted object not found")
-		fmt.Println("this is an err, middleware/profile/deleteTag line 54")
-		newArr = usr.Tags
-	} else if (target + 1) < len(usr.Tags) {
-		newArr = append(usr.Tags[:target], usr.Tags[(target+1):]...)
-	} else {
-		newArr = usr.Tags[:target]
-	}
-
-	err = post.UpdateUser(client.Eclient, ID, "Tags", newArr)
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println("this is an err: middleware/profile/deleteTag line 31")
 	}
 }
