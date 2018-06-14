16681e664627fb04406fe34a514ea10646ef065a
mrb721 <mrb_2059@yahoo.com>
2018-06-14 15:27:34 -0400

    project privileges!!!!1!

--- middleware/settings/projectLogo.go
+++ middleware/settings/projectLogo.go
@@ -21,26 +21,32 @@ func ProjectLogo(w http.ResponseWriter, r *http.Request) {
 	clientFile, header, err := r.FormFile("raw-image")
 	blob := r.FormValue("image-data")
 
-	//Getting projectID
-	projID := r.FormValue("projectID")
-	proj, err := get.ProjectByID(eclient, projID)
+	//Getting projectID and member
+	// projID := r.FormValue("projectID")
+	// proj, err := get.ProjectByID(eclient, projID)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	proj, member, err := get.ProjAndMember(eclient, r.FormValue("projectID"), test1.(string))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("err: middleware/settings/projectlogo line 33\n", err)
 	}
 
-	//Checking if image is valid by checking the first 512 bytes for correct image signature
-	buffer := make([]byte, 512)
-	_, _ = clientFile.Read(buffer)
-	defer clientFile.Close()
-	if http.DetectContentType(buffer)[0:5] == "image" || header.Size == 0 {
-		err = uses.ChangeProjectLogo(eclient, projID, blob)
-		if err != nil {
-			fmt.Println(err)
+	if uses.HasPrivilege("icon", proj, member) {
+		//Checking if image is valid by checking the first 512 bytes for correct image signature
+		buffer := make([]byte, 512)
+		_, _ = clientFile.Read(buffer)
+		defer clientFile.Close()
+		if http.DetectContentType(buffer)[0:5] == "image" || header.Size == 0 {
+			err = uses.ChangeProjectLogo(eclient, projID, blob)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("err: middleware/settings/projectLogo invalid file upload\n", err)
 		}
-	} else {
-		fmt.Println("err: middleware/settings/projectLogo invalid file upload\n", err)
 	}
-
 	http.Redirect(w, r, "/ProjectSettings/"+proj.URLName, http.StatusFound)
 	return
 
