b84a00d9a2c747b37fd418d88d35000aa5ed979d
Jonchow77 <Jonchow77@gmail.com>
2018-06-21 16:14:43 -0400

    Fixed sendPasswordResetEmail

--- uses/sendPasswordResetEmail.go
+++ uses/sendPasswordResetEmail.go
@@ -5,16 +5,33 @@ import (
 	"log"
 	"net/smtp"
 
+	get "github.com/sea350/ustart_go/get/user"
+	"github.com/sea350/ustart_go/middleware/client"
+	post "github.com/sea350/ustart_go/post/user"
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
-//SendPasswordResetEmail ... Sends password reset token link to user
-//Requires the user's email address
+//SendPasswordResetEmail ... Sends password reset token link to user and saves token to their AuthenticationCode
+//Requires a valid user email address
 //Returns if the email failed to send
 func SendPasswordResetEmail(eclient *elastic.Client, email string) {
+	userID, err := get.UserIDByEmail(client.Eclient, email)
+	if err != nil {
+		fmt.Println("Error ustart_go/uses/resendVerificationEmail line 16: Unable to retreive userID by email")
+		fmt.Println(err)
+		return
+	}
+
 	token, err := GenerateRandomString(32)
 	if err != nil {
 		fmt.Println("Error ustart_go/uses/resendVerificationEmail line 16: Error generating token")
+		fmt.Println(err)
+		return
+	}
+
+	err = post.UpdateUser(eclient, userID, "AuthenticationCode", token)
+	if err != nil {
+		fmt.Println("err: Unable to retreive userID by email")
 		fmt.Println(err)
 		return
 	}
@@ -31,5 +48,4 @@ func SendPasswordResetEmail(eclient *elastic.Client, email string) {
 		log.Printf("smtp error: %s", err4)
 		return
 	}
-	SendEmail(email, token)
 }
