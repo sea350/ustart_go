9404d715c6811997f6978f11e2847d6d60befdfe
mrb721 <mrb_2059@yahoo.com>
2018-06-21 18:30:33 -0400

    IF YOU STRIKE ME DOWN....

--- get/project/getProjAndMember.go
+++ get/project/getProjAndMember.go
@@ -20,6 +20,7 @@ func ProjAndMember(eclient *elastic.Client, projID string, memberID string) (typ
 	}
 
 	var retMember types.Member
+	retMember.Role = -1
 	for mem := range project.Members {
 		if project.Members[mem].MemberID == memberID {
 			retMember = project.Members[mem]
