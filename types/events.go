774a269c6060e817b51d8c871d22a2f1ad1afdea
Tarek <thassoun000@gmail.com>
2018-06-26 16:10:04 -0400

    Event fix

--- types/events.go
+++ types/events.go
@@ -7,6 +7,7 @@ import (
 //invite only, publicly viewable, whitelist, blacklist, settings, members, privileges, title, description, creation, event date, option for
 //creator to cancel, visibility boolean, widgets, get/post, attending/invited/not going, members list and guest list
 
+//EventPrivileges ...
 type EventPrivileges struct {
 	RoleName     string `json:"RoleName"`
 	RoleID       int    `json:"RoleID"`
@@ -19,12 +20,14 @@ type EventPrivileges struct {
 	Tags         bool   `json:"Tags"`
 }
 
+//EventGuests ...
 type EventGuests struct {
 	GuestID string `json:"GuestID"`
 	Status  int    `json:"Status"` //Marks whether they are invited/going/not going, 0 for invited, 1 for going, 2 for not
-	Visible bool   `json:"Visible'`
+	Visible bool   `json:"Visible"`
 }
 
+//EventMembers ...
 type EventMembers struct {
 	MemberID string    `json:"MemberID"`
 	Role     int       `json:"Role"`
@@ -32,23 +35,27 @@ type EventMembers struct {
 	Title    string    `json:"Title"`
 	Visible  bool      `json:"Visible"`
 }
+
+//Events ...
 type Events struct {
-	Name          string         `json:"Name"`
-	URLName       string         `json:"URLName"`
-	Description   []rune         `json:"Description"`
-	Members       []EventMembers `json:"Members"`
-	Guests        []EventGuests  `json:"Guests"`
-	Location      LocStruct      `json:"Location"`
-	EventDate     time.Time      `json:"EventDate"`
-	CreationDate  time.Time      `json:"CreationDate"`
-	Widgets       []Widget       `json:"Widgets"`
-	Whitelist     string         `json:"Whitelist"`
-	Blacklist     string         `json:"Blacklist"`
-	Status        bool           `json:"Status"` //Whether this event is still ongoing or cancelled
-	Public        bool           `json:"Public"` //Whether this event is publicly viewable or invite-only viewable
-	Avatar        string         `json:"Avatar"`
-	CroppedAvatar string         `json:"CropAvatar"`
-	Banner        string         `json:"Banner"`
-	MemberReqSent []string       `json:"MemberReqSent"`
+	Name              string            `json:"Name"`
+	Category          string            `json:"Category"`
+	URLName           string            `json:"URLName"`
+	Description       []rune            `json:"Description"`
+	Members           []EventMembers    `json:"Members"`
+	Guests            []EventGuests     `json:"Guests"`
+	Location          LocStruct         `json:"Location"`
+	EventDate         time.Time         `json:"EventDate"`
+	CreationDate      time.Time         `json:"CreationDate"`
+	Widgets           []Widget          `json:"Widgets"`
+	Whitelist         []string          `json:"Whitelist"`
+	Blacklist         []string          `json:"Blacklist"`
+	Status            bool              `json:"Status"` //Whether this event is still ongoing or cancelled
+	Public            bool              `json:"Public"` //Whether this event is publicly viewable or invite-only viewable
+	Avatar            string            `json:"Avatar"`
+	CroppedAvatar     string            `json:"CropAvatar"`
+	Banner            string            `json:"Banner"`
+	MemberReqSent     []string          `json:"MemberReqSent"`
+	PrivilegeProfiles []EventPrivileges `json:"PrivilegeProfiles"`
 	//MemberReqReceived []string       `json:"MemberReqReceived"` We probably don't want this
 }
