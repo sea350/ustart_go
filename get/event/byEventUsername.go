5bb3efef6d678b1a52c0c44a9bc4e4b636028a2d
Tarek <thassoun000@gmail.com>
2018-06-28 15:33:59 -0400

    Get Event Done for real, for now

---
+++ get/event/byEventUsername.go
@@ -0,0 +1,36 @@
+package get
+
+import (
+	"context"
+	"strings"
+
+	globals "github.com/sea350/ustart_go/globals"
+	types "github.com/sea350/ustart_go/types"
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+//EventByUsername ...
+func EventByUsername(eclient *elastic.Client, username string) (types.Events, error) {
+	ctx := context.Background()
+
+	termQuery := elastic.NewTermQuery("Username", strings.ToLower(username))
+	searchResult, err := eclient.Search().
+		Index(globals.EventIndex).
+		Query(termQuery).
+		Do(ctx)
+
+	var evnt types.Events
+	if err != nil {
+		return evnt, err
+	}
+
+	var result string
+
+	for _, element := range searchResult.Hits.Hits {
+		result = element.Id
+		break
+	}
+
+	evnt, err = EventByID(eclient, result)
+	return evnt, err
+}
