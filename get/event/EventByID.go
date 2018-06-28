3f1cc4174d1033dad94753e5cbb6f353a0ff0ea6
Tarek <thassoun000@gmail.com>
2018-06-28 13:05:28 -0400

    event get chapter 1

---
+++ get/event/EventByID.go
@@ -0,0 +1,31 @@
+package get
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	globals "github.com/sea350/ustart_go/globals"
+	types "github.com/sea350/ustart_go/types"
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+//EventByID ... Pulls from ES an Event (and an error)
+func EventByID(eclient *elastic.Client, eventID string) (types.Events, error) {
+	var evnt types.Events
+
+	ctx := context.Background()
+
+	searchResult, err := eclient.Get().
+		Index(globals.EventIndex).
+		Type(globals.EventType).
+		Id(eventID).
+		Do(ctx)
+
+	err = json.Unmarshal(*searchResult.Source, &evnt)
+	if err != nil {
+		fmt.Printf("Err: %s\n", err.Error())
+	}
+
+	return evnt, err
+}
