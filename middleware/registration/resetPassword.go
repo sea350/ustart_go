c210cc50d4c5c65bdc44e9f25bff05ab846bab18
Jonchow77 <Jonchow77@gmail.com>
2018-06-29 12:27:28 -0400

    Fixing password reset

--- middleware/registration/resetPassword.go
+++ middleware/registration/resetPassword.go
@@ -16,12 +16,11 @@ import (
 //Requires the user's email address
 //Returns if the email failed to send
 func ResetPassword(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
 	session, _ := client.Store.Get(r, "session_please")
 	test1, _ := session.Values["DocID"]
 	if test1 != nil {
 		fmt.Println("Test1:", test1)
-		http.Redirect(w, r, "/~", http.StatusFound)
+		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
 
