9a49b12150698048248f533e76695aceb9c83252
Gitnil <garudyne@gmail.com>
2018-06-30 16:42:11 -0400

    Form Values for Event Form

--- middleware/event/ViewEvent.go
+++ middleware/event/ViewEvent.go
@@ -36,6 +36,7 @@ func StartEvent(w http.ResponseWriter, r *http.Request) {
 	client.RenderTemplate(w, r, "eventStart", cs)
 }
 
+//AddEvent ... append event to database
 func AddEvent(w http.ResponseWriter, r *http.Request) {
 	session, _ := client.Store.Get(r, "session_please")
 	test1, _ := session.Values["DocID"]
@@ -43,9 +44,18 @@ func AddEvent(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/~", http.StatusFound)
 		return
 	}
+	r.ParseForm()
+
+	title := r.FormValue("title")
+	dateStart := r.FormValue("dateStart")
+	dateEnd := r.FormValue("dateEnd")
+	country := r.FormValue("country")
+	state := r.FormValue("state")
+	city := r.FormValue("city")
+	zip := r.FormValue("zip")
+
 	cs := client.ClientSide{}
 
-	client.RenderSidebar(w, r, "template2-nil")
-	client.RenderSidebar(w, r, "leftnav-nil")
-	client.RenderTemplate(w, r, "events", cs)
+	http.Redirect(w, r, "/Event/", http.StatusFound)
+	http.Redirect(w, r, "/~", http.StatusFound)
 }
