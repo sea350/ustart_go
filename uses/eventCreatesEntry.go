33c17c09460ef4c94008014089214552fc7ba813
Tarek <thassoun000@gmail.com>
2018-07-03 11:43:14 -0400

    entry uses functions done for now

--- uses/eventCreatesEntry.go
+++ uses/eventCreatesEntry.go
@@ -1,3 +1,75 @@
 package uses
 
-//Will get to this in time
+import (
+	"time"
+
+	postEntry "github.com/sea350/ustart_go/post/entry"
+	post "github.com/sea350/ustart_go/post/event"
+	"github.com/sea350/ustart_go/types"
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+//EventCreatesEntry ... creates a new entry for events and handles logic/parallel arrays
+func EventCreatesEntry(eclient *elastic.Client, eventID string, posterID string, newContent []rune) (string, error) {
+	createdEntry := types.Entry{}
+	createdEntry.PosterID = posterID
+	createdEntry.Classification = 0
+	createdEntry.Content = newContent
+	createdEntry.TimeStamp = time.Now()
+	createdEntry.Visible = true
+
+	entryID, err := postEntry.IndexEntry(eclient, createdEntry)
+	if err != nil {
+		return entryID, err
+	}
+
+	err = post.AppendEntryID(eclient, eventID, entryID)
+	return entryID, err
+}
+
+//EventCreatesReply ... creates a new reply entry for events and handles logic/parallel arrays
+func EventCreatesReply(eclient *elastic.Client, eventID string, replyID string, posterID string, newContent []rune) error {
+	createdEntry := types.Entry{}
+	createdEntry.PosterID = posterID
+	createdEntry.Classification = 1
+	createdEntry.Content = newContent
+	createdEntry.TimeStamp = time.Now()
+	createdEntry.Visible = true
+
+	entryID, err := postEntry.IndexEntry(eclient, createdEntry)
+	if err != nil {
+		return err
+	}
+
+	err = post.AppendEntryID(eclient, eventID, entryID)
+	if err != nil {
+		return err
+	}
+
+	err = postEntry.AppendReplyID(eclient, entryID, replyID)
+	return err
+}
+
+//EntryCreatesShare ... creates a new share entry for events and handles logic/parallel arrays
+func EventCreatesShare(eclient *elastic.Client, eventID string, replyID string, posterID string, newContent []rune) error {
+	createdEntry := types.Entry{}
+	createdEntry.PosterID = posterID
+	createdEntry.Classification = 2
+	createdEntry.Content = newContent
+	createdEntry.TimeStamp = time.Now()
+	createdEntry.Visible = true
+
+	entryID, err := postEntry.IndexEntry(eclient, createdEntry)
+	if err != nil {
+		return err
+	}
+
+	err = post.AppendEntryID(eclient, eventID, entryID)
+	if err != nil {
+		return err
+	}
+
+	err = postEntry.AppendShareID(eclient, entryID, replyID)
+
+	return err
+}
