0538ea9f7cd07e731f6adef54356ccf6a32fde47
Tarek <thassoun000@gmail.com>
2018-07-05 13:23:38 -0400

    event app middleware

---
+++ app_middleware/event/response.go
@@ -0,0 +1,25 @@
+package event
+
+import (
+	"fmt"
+
+	"github.com/sea350/ustart_go/types"
+)
+
+type response struct {
+	Successful bool         `json:"Successful"`
+	Event      types.Events `json:"Event"`
+	ErrMsg     error        `json:"Error"`
+}
+
+func (r *response) update(success bool, em error, evnt types.Events) {
+
+	r.ErrMsg = em
+	r.Successful = success
+	r.Event = evnt
+
+	if em != nil && em.Error() != "Password is incorrect" {
+		fmt.Println("Error" + em.Error())
+		panic(em)
+	}
+}
