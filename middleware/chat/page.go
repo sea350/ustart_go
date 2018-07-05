ec1709c6f9aa075fdaa8500f3d5f11861060f8fe
sea350 <sea350@nyu.edu>
2018-07-05 12:39:57 -0400

    just work dammit

--- middleware/chat/page.go
+++ middleware/chat/page.go
@@ -11,14 +11,14 @@ func Page(w http.ResponseWriter, r *http.Request) {
 	cs := client.ClientSide{}
 	client.RenderSidebar(w, r, "template2-nil")
 	client.RenderTemplate(w, r, "cuzsteventoldmeto", cs)
-
+	chatID := r.URL.Path[4:]
 	// var h = hubAlt{
 	// 	broadcast:  make(chan messageAlt),
 	// 	register:   make(chan subscription),
 	// 	unregister: make(chan subscription),
 	// 	rooms:      make(map[string]map[*connection]bool),
 	// }
-	// go handleMessages()
-	go ServeWs(w, r)
+	go handleMessages(chatID)
+	//go ServeWs(w, r)
 	// go h.Run()
 }
