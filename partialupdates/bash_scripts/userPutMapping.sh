013ec172edad1f608045c6fe40061ceaf4f13f80
mrb721 <mrb_2059@yahoo.com>
2018-07-17 15:11:38 -0400

    put mapping

--- partialupdates/bash_scripts/userPutMapping.sh
+++ partialupdates/bash_scripts/userPutMapping.sh
@@ -10,8 +10,9 @@ CURL_BASE='http://'$USER':'$PASS'@'$URL':'$PORT
  
 #curl -XPOST "$CURL_BASE""$TEST_USER"'/0?pretty=true' -H "$JSON" -d "$USER_TEMPLATE"
 # curl -XGET $CURL_BASE'/_cat/indices?v&pretty=true&s=index'
+
 echo $INDEX
-curl -XPUT  "$CURL_BASE""$INDEX"'/_mapping/_doc' -H 'Content-Type: application/json' -d'
+curl -XPUT  "$CURL_BASE""$INDEX"'/_mapping/_doc' -H "$JSON" -d'
 {
   "properties": {
     "USER": {
@@ -26,3 +27,5 @@ curl -XPUT  "$CURL_BASE""$INDEX"'/_mapping/_doc' -H 'Content-Type: application/j
   }
 }
 '
+
+
