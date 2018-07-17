528b4f9228700000d224f416b1883e896bf9e99c
mrb721 <mrb_2059@yahoo.com>
2018-07-17 13:44:10 -0400

    debug

--- partialupdates/bash_scripts/userPutMapping.sh
+++ partialupdates/bash_scripts/userPutMapping.sh
@@ -2,7 +2,7 @@ chmod +x ./userPutMapping.sh
 USER='elastic'
 PASS='elasticpassword'
 URL='localhost'
-PORT='9200'
+PORT='9200/'
 JSON='Content-Type:application/json'
 INDEX='test-user_data/USER'
 CURL_BASE='http://'$USER':'$PASS'@'$URL':'$PORT
@@ -11,7 +11,7 @@ CURL_BASE='http://'$USER':'$PASS'@'$URL':'$PORT
 #curl -XPOST "$CURL_BASE""$TEST_USER"'/0?pretty=true' -H "$JSON" -d "$USER_TEMPLATE"
 # curl -XGET $CURL_BASE'/_cat/indices?v&pretty=true&s=index'
 echo $INDEX
-curl -X PUT  $CURL_BASE/$INDEX/'_mapping/_doc' -H 'Content-Type: application/json' -d'
+curl -X PUT  "$CURL_BASE""$INDEX"'/_mapping/_doc' -H 'Content-Type: application/json' -d'
 {
   "properties": {
     "USER": {
