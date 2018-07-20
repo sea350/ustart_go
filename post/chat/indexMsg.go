c37c7e2d913ffc1922b7b93eb69d7e66f607d13d
mrb721 <mrb_2059@yahoo.com>
2018-07-20 16:36:02 -0400

    fuck this shit

--- post/chat/indexMsg.go
+++ post/chat/indexMsg.go
@@ -17,7 +17,7 @@ func IndexMsg(eclient *elastic.Client, newMsg types.Message) (string, error) {
 	//RETURNS AN error and the new chat's ID IF SUCESSFUL error = nil
 	ctx := context.Background()
 	var msgID string
-	exists, err := eclient.IndexExists(globals.MsgType).Do(ctx)
+	exists, err := eclient.IndexExists(globals.MsgIndex).Do(ctx)
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		dir, _ := os.Getwd()
