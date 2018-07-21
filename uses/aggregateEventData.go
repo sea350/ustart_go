616eea986e78392e489ac918e0340651a854f7fc
Gitnil <garudyne@gmail.com>
2018-07-21 19:17:33 -0400

    Debug Everything About EventURL

--- uses/aggregateEventData.go
+++ uses/aggregateEventData.go
@@ -3,7 +3,6 @@ package uses
 import (
 	"log"
 	"os"
-	"fmt"
 
 	getEvent "github.com/sea350/ustart_go/get/event"
 	types "github.com/sea350/ustart_go/types"
@@ -16,36 +15,28 @@ func AggregateEventData(eclient *elastic.Client, url string, viewerID string) (t
 	var eventData types.EventAggregate
 	eventData.RequestAllowed = true
 	
-	fmt.Println("1")
 	data, err := getEvent.EventByURL(eclient, url)
-	fmt.Println("2")
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		dir, _ := os.Getwd()
 		log.Println(dir, err)
 	}
-	fmt.Println("3")
 	eventData.EventData = data
 
-	fmt.Println("4")
 	id, err := getEvent.EventIDByURL(eclient, url)
-	fmt.Println("5")
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		dir, _ := os.Getwd()
 		log.Println(dir, err)
 	}
-	fmt.Println("6")
 	eventData.DocID = id
 
-	fmt.Println("7")
 	//Remember to load widgets seperately
 	//Remember to load wall posts seperately
 
 	for _, member := range data.Members {
 		id := member.MemberID
 		mem, err := ConvertUserToFloatingHead(eclient, id)
-		fmt.Println("8")
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
 			dir, _ := os.Getwd()
@@ -57,14 +48,11 @@ func AggregateEventData(eclient *elastic.Client, url string, viewerID string) (t
 			eventData.RequestAllowed = false
 		}
 	}
-	fmt.Println("9")
 	for _, receivedReq := range eventData.EventData.MemberReqReceived {
-		fmt.Println("10")
 		if receivedReq == viewerID {
 			eventData.RequestAllowed = false
 		}
 	}
-	fmt.Println("11")
 
 	return eventData, err
 }
