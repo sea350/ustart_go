5669c58e4a191f45c773fba2a07143f45607aa00
mrb721 <mrb_2059@yahoo.com>
2018-07-21 13:23:14 -0400

    debug

--- middleware/widget/processWidgetForm.go
+++ middleware/widget/processWidgetForm.go
@@ -58,7 +58,7 @@ func ProcessWidgetForm(r *http.Request) (types.Widget, error) {
 		// regX := regexp.MustCompile(`https?:\/\/(www\.)?instagram\.com\/([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)`)
 		regX := regexp.MustCompile(`(https?:\/\/www\.)?instagram\.com(\/p\/\w+\/?)`)
 
-		fmt.Println("THE REGX", regX.String)
+		// fmt.Println("THE REGX", regX.String)
 		if !regX.MatchString(insta) {
 			return newWidget, errors.New(`Unusable Instagram URL`)
 		} //Check valid URL
