7e478c5136b292e4d6f68175a9fdc1b84ee2d490
sea350 <sea350@nyu.edu>
2018-07-24 15:38:20 -0400

    barebones

--- middleware/chat/initialChat.go
+++ middleware/chat/initialChat.go
@@ -7,8 +7,6 @@ import (
 	"net/http"
 	"os"
 
-	getChat "github.com/sea350/ustart_go/get/chat"
-	"github.com/sea350/ustart_go/types"
 	"github.com/sea350/ustart_go/uses"
 
 	"github.com/sea350/ustart_go/middleware/client"
@@ -25,7 +23,7 @@ func InitialChat(w http.ResponseWriter, r *http.Request) {
 
 	chatURL := r.FormValue("chatUrl")
 
-	valid, actualChatID, otherUsr, err := uses.ChatVerifyURL(client.Eclient, chatURL, docID.(string))
+	valid, actualChatID, _, err := uses.ChatVerifyURL(client.Eclient, chatURL, docID.(string))
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		dir, _ := os.Getwd()
@@ -35,7 +33,7 @@ func InitialChat(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	heads := make(map[string]types.FloatingHead)
+	//heads := make(map[string]types.FloatingHead)
 
 	if actualChatID != `` {
 		_, msgs, err := uses.ChatLoad(client.Eclient, actualChatID, 0, 30)
@@ -62,41 +60,41 @@ func InitialChat(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintln(w, string(data))
 
-		chat, err := getChat.ConvoByID(client.Eclient, actualChatID)
-		if err != nil {
-			log.SetFlags(log.LstdFlags | log.Lshortfile)
-			dir, _ := os.Getwd()
-			log.Println(dir, err)
-			return
-		}
+		// chat, err := getChat.ConvoByID(client.Eclient, actualChatID)
+		// if err != nil {
+		// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+		// 	dir, _ := os.Getwd()
+		// 	log.Println(dir, err)
+		// 	return
+		// }
 
-		for idx := range chat.Eavesdroppers {
-			head, err := uses.ConvertUserToFloatingHead(client.Eclient, chat.Eavesdroppers[idx].DocID)
-			if err != nil {
-				log.SetFlags(log.LstdFlags | log.Lshortfile)
-				dir, _ := os.Getwd()
-				log.Println(dir, err)
-			}
-			heads[chat.Eavesdroppers[idx].DocID] = head
-		}
+		// for idx := range chat.Eavesdroppers {
+		// 	head, err := uses.ConvertUserToFloatingHead(client.Eclient, chat.Eavesdroppers[idx].DocID)
+		// 	if err != nil {
+		// 		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		// 		dir, _ := os.Getwd()
+		// 		log.Println(dir, err)
+		// 	}
+		// 	heads[chat.Eavesdroppers[idx].DocID] = head
+		//}
 
-	} else {
-		head, err := uses.ConvertUserToFloatingHead(client.Eclient, otherUsr)
-		if err != nil {
-			log.SetFlags(log.LstdFlags | log.Lshortfile)
-			dir, _ := os.Getwd()
-			log.Println(dir, err)
-		}
-		heads[otherUsr] = head
+	} //else {
+	// head, err := uses.ConvertUserToFloatingHead(client.Eclient, otherUsr)
+	// if err != nil {
+	// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	// 	dir, _ := os.Getwd()
+	// 	log.Println(dir, err)
+	// }
+	// heads[otherUsr] = head
 
-		head, err = uses.ConvertUserToFloatingHead(client.Eclient, docID.(string))
-		if err != nil {
-			log.SetFlags(log.LstdFlags | log.Lshortfile)
-			dir, _ := os.Getwd()
-			log.Println(dir, err)
-		}
-		heads[docID.(string)] = head
-	}
+	// head, err = uses.ConvertUserToFloatingHead(client.Eclient, docID.(string))
+	// if err != nil {
+	// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	// 	dir, _ := os.Getwd()
+	// 	log.Println(dir, err)
+	// }
+	// heads[docID.(string)] = head
+	//}
 
 	// data, err := json.Marshal(heads)
 	// if err != nil {
