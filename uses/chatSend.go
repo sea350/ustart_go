98546bcc4d82813191b11b73a6cae271bb3af70b
sea350 <sea350@nyu.edu>
2018-07-26 16:30:33 -0400

    removed debug text

--- uses/chatSend.go
+++ uses/chatSend.go
@@ -54,8 +54,6 @@ func ChatSend(eclient *elastic.Client, msg types.Message) ([]string, error) {
 			log.Println(err)
 			return notifyThese, err
 		}
-		log.SetFlags(log.LstdFlags | log.Lshortfile)
-		log.Println("debug text: " + pID)
 		err = postChat.AppendToProxy(eclient, pID, msg.ConversationID)
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
