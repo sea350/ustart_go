79cfa5cb37c594c3e5220a7ee186b852b787c6f4
sea350 <sea350@nyu.edu>
2018-08-03 14:24:10 -0400

    REEEEEEEEEEEEE

---
+++ middleware/chat/ajaxLoadMoreChat.go
@@ -0,0 +1,61 @@
+package chat
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/sea350/ustart_go/uses"
+
+	"github.com/sea350/ustart_go/middleware/client"
+)
+
+//AjaxLoadMoreChat ... loads more chat
+//Designed for ajax
+func AjaxLoadMoreChat(w http.ResponseWriter, r *http.Request) {
+	session, _ := client.Store.Get(r, "session_please")
+	docID, _ := session.Values["DocID"]
+	if docID == nil {
+		return
+	}
+
+	chatID := r.FormValue("chatID")
+	idxStr := r.FormValue("index")
+	idx, err := strconv.Atoi(idxStr)
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+		return
+	}
+
+	valid, actualChatID, _, err := uses.ChatVerifyURL(client.Eclient, chatID, docID.(string))
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+	}
+	if !valid {
+		return
+	}
+
+	newIdx, msgs, err := uses.ChatLoad(client.Eclient, actualChatID, 0, idx)
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+		return
+	}
+
+	sendThis := make(map[string]interface{})
+	sendThis["newIndex"] = newIdx
+	sendThis["messages"] = msgs
+
+	data, err := json.Marshal(sendThis)
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		dir, _ := os.Getwd()
+		log.Println(dir, err)
+	}
+	fmt.Fprintln(w, string(data))
+}
