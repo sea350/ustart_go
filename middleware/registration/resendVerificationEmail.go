59bb175f327e2550d6f50340a3565154ca253442
Jonchow77 <Jonchow77@gmail.com>
2018-08-09 15:51:46 -0400

    Added resend verification email to GoStart2

--- middleware/registration/resendVerificationEmail.go
+++ middleware/registration/resendVerificationEmail.go
@@ -7,7 +7,7 @@ import (
 	uses "github.com/sea350/ustart_go/uses"
 )
 
-func ResendEmailVerification(w http.ResponseWriter, r *http.Request) {
+func ResendVerificationEmail(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("email")
 	uses.SendVerificationEmail(client.Eclient, email)
 }
