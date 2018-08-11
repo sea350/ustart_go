55add5e4b50c603fb20b4d997716fcdfa97b2e52
sea350 <sea350@nyu.edu>
2018-08-11 16:39:00 -0400

    work already

--- middleware/profile/Like.go
+++ middleware/profile/Like.go
@@ -17,21 +17,26 @@ func Like(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	postid := r.FormValue("PostID")
+	postID := r.FormValue("PostID")
+	if postID == `` {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println("WARNING: no post Id passed in")
+		return
+	}
 
-	likeStatus, err := uses.IsLiked(client.Eclient, postid, docID.(string))
+	likeStatus, err := uses.IsLiked(client.Eclient, postID, docID.(string))
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		log.Println(err)
 	}
 	if likeStatus == true {
-		err := uses.UserUnlikeEntry(client.Eclient, postid, docID.(string))
+		err := uses.UserUnlikeEntry(client.Eclient, postID, docID.(string))
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
 			log.Println(err)
 		}
 	} else {
-		err := uses.UserLikeEntry(client.Eclient, postid, docID.(string))
+		err := uses.UserLikeEntry(client.Eclient, postID, docID.(string))
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
 			log.Println(err)
