14dec154540611ce75a85c5add2d7198664f2664
sea350 <sea350@nyu.edu>
2018-08-11 16:27:17 -0400

    i crye

--- middleware/profile/Like.go
+++ middleware/profile/Like.go
@@ -11,27 +11,27 @@ import (
 //Like ... likes a post, designed for ajax
 func Like(w http.ResponseWriter, r *http.Request) {
 	session, _ := client.Store.Get(r, "session_please")
-	test1, _ := session.Values["DocID"]
-	if test1 == nil {
+	docID, _ := session.Values["DocID"]
+	if docID == nil {
 		http.Redirect(w, r, "/~", http.StatusFound)
 		return
 	}
 
 	postid := r.FormValue("PostID")
 
-	likeStatus, err := uses.IsLiked(client.Eclient, postid, session.Values["DocID"].(string))
+	likeStatus, err := uses.IsLiked(client.Eclient, postid, docID.(string))
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		log.Println(err)
 	}
 	if likeStatus == true {
-		err := uses.UserUnlikeEntry(client.Eclient, postid, session.Values["DocID"].(string))
+		err := uses.UserUnlikeEntry(client.Eclient, postid, docID.(string))
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
 			log.Println(err)
 		}
 	} else {
-		err := uses.UserLikeEntry(client.Eclient, postid, session.Values["DocID"].(string))
+		err := uses.UserLikeEntry(client.Eclient, postid, docID.(string))
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
 			log.Println(err)
