487010ae490b8cf6d78e316a1cf5214565e0d88b
sea350 <sea350@nyu.edu>
2018-08-11 16:44:01 -0400

    ok

--- middleware/profile/Like.go
+++ middleware/profile/Like.go
@@ -1,9 +1,12 @@
 package profile
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	get "github.com/sea350/ustart_go/get/entry"
 	client "github.com/sea350/ustart_go/middleware/client"
 	uses "github.com/sea350/ustart_go/uses"
 )
@@ -17,29 +20,42 @@ func Like(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	postID := r.FormValue("PostID")
-	if postID == `` {
+	entryID := r.FormValue("PostID")
+	if entryID == `` {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
-		log.Println("WARNING: no post Id passed in")
+		log.Println("WARNING: no entry Id passed in")
 		return
 	}
 
-	likeStatus, err := uses.IsLiked(client.Eclient, postID, docID.(string))
+	likeStatus, err := uses.IsLiked(client.Eclient, entryID, docID.(string))
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		log.Println(err)
 	}
 	if likeStatus == true {
-		err := uses.UserUnlikeEntry(client.Eclient, postID, docID.(string))
+		err := uses.UserUnlikeEntry(client.Eclient, entryID, docID.(string))
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
 			log.Println(err)
 		}
 	} else {
-		err := uses.UserLikeEntry(client.Eclient, postID, docID.(string))
+		err := uses.UserLikeEntry(client.Eclient, entryID, docID.(string))
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
 			log.Println(err)
 		}
 	}
+
+	entry, err := get.EntryByID(client.Eclient, entryID)
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+	}
+
+	data, err := json.Marshal(len(entry.Likes))
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+	}
+	fmt.Fprintln(w, string(data))
 }
