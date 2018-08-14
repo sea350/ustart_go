a7894900efdc91997b47d8e27d909035e2ec0173
gpDA <geonpy@likelion.org>
2018-08-14 16:15:21 -0400

    add streetVis

--- types/location.go
+++ types/location.go
@@ -10,4 +10,6 @@ type LocStruct struct {
 	CityVis    bool   `json:"CityVis"`
 	Zip        string `json:"Zip"`
 	ZipVis     bool   `json:"ZipVis"`
+	Street     string `json:"Street"`
+	StreetVis  bool   `json:"StreetVis"`
 }
