db01f0d8143d858d2bb3b7d82d1d90be90dc34e9
sea350 <sea350@nyu.edu>
2018-08-16 16:38:08 -0400

    WIP

---
+++ get/notification/notificationByID.go
@@ -0,0 +1,33 @@
+package get
+
+import (
+	"context"
+	"encoding/json"
+
+	globals "github.com/sea350/ustart_go/globals"
+	types "github.com/sea350/ustart_go/types"
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+//NotificationByID ...
+func NotificationByID(eclient *elastic.Client, notifID string) (types.Notification, error) {
+
+	var notif types.Notification
+
+	ctx := context.Background()         //intialize context background
+	searchResult, err := eclient.Get(). //Get returns doc type, index, etc.
+						Index(globals.NotificationIndex).
+						Id(notifID).
+						Do(ctx)
+	if err != nil {
+		return notif, err
+	}
+
+	Err := json.Unmarshal(*searchResult.Source, &notif)
+	if Err != nil {
+		return notif, Err
+	} //forward error
+
+	return notif, Err
+
+}
