8a360072f4094c1863319d2d9473e91159c0285b
sea350 <sea350@nyu.edu>
2018-08-21 13:39:58 -0400

    pog

--- get/notification/proxyNotificationByUserID.go
+++ get/notification/proxyNotificationByUserID.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"log"
 	"strings"
 
 	globals "github.com/sea350/ustart_go/globals"
@@ -15,20 +16,45 @@ import (
 func ProxyNotificationByUserID(eclient *elastic.Client, userID string) (types.ProxyNotifications, error) {
 
 	ctx := context.Background()
+	var proxy types.ProxyNotifications
 
+	exists, err := eclient.IndexExists(globals.ProxyNotifIndex).Do(ctx)
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+	}
+	if !exists {
+		_, err := eclient.CreateIndex(globals.ProxyNotifIndex).Do(ctx)
+		if err != nil {
+			log.SetFlags(log.LstdFlags | log.Lshortfile)
+			log.Println(err)
+			return proxy, err
+		}
+	}
 	termQuery := elastic.NewTermQuery("DocID", strings.ToLower(userID))
 	searchResult, err := eclient.Search().
 		Index(globals.ProxyNotifIndex).
 		Query(termQuery).
 		Do(ctx)
 
-	var proxy types.ProxyNotifications
 	if err != nil {
 		return proxy, err
 	}
 
 	if searchResult.TotalHits() == 0 {
-		return proxy, errors.New("No results, proxy ID does not exist")
+		proxy.Settings.Default()
+		proxy.DocID = userID
+
+		_, err := eclient.Index().
+			Index(globals.ProxyNotifIndex).
+			BodyJson(proxy).
+			Do(ctx)
+
+		if err != nil {
+			return types.ProxyNotifications{}, err
+		} else {
+			return proxy, nil
+		}
 	}
 	if searchResult.TotalHits() > 1 {
 		return proxy, errors.New("multiple proxies found")
