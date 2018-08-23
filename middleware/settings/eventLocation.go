981077a67ced1140c0e7be61303ed129c797ac02
gpDA <geonpy@likelion.org>
2018-08-23 13:54:43 -0400

    changed location

--- middleware/settings/eventLocation.go
+++ middleware/settings/eventLocation.go
@@ -21,9 +21,12 @@ func EventLocation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	r.ParseForm()
+	Sdate := r.FormValue("startDate")
+	Edate := r.FormValue("endDate")
 	country := r.FormValue("country")
 	state := r.FormValue("state")
 	city := r.FormValue("city")
+	street := r.FormValue("street")
 	zip := r.FormValue("zip")
 	//   fmt.Println(blob)
 
@@ -31,7 +34,7 @@ func EventLocation(w http.ResponseWriter, r *http.Request) {
 	evnt, err := get.EventByID(eclient, evntID)
 	//fmt.Println(reflect.TypeOf(blob))
 	//TODO: DocID
-	err = uses.ChangeEventLocation(eclient, evntID, country, state, city, zip)
+	err = uses.ChangeEventLocation(eclient, evntID, Sdate, Edate, country, state, city, street, zip)
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		dir, _ := os.Getwd()
