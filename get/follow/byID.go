f573000d7bc9ff50785cf8d4b526792787dfc0f0
mrb721 <mrb_2059@yahoo.com>
2018-08-23 17:28:39 -0400

    follow logic

--- get/follow/byID.go
+++ get/follow/byID.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"log"
 	"strings"
 
 	globals "github.com/sea350/ustart_go/globals"
 	types "github.com/sea350/ustart_go/types"
 	elastic "gopkg.in/olivere/elastic.v5"
-	//post "github.com/sea350/ustart_go/post"
 )
 
 //ByID ...
@@ -31,7 +31,55 @@ func ByID(eclient *elastic.Client, userID string) (string, types.Follow, error) 
 	if searchResult.Hits.TotalHits > 2 {
 		return "", foll, errors.New("More than one result found")
 	} else if searchResult.Hits.TotalHits < 1 {
-		return "", foll, errors.New("No results found")
+		exists, err := eclient.IndexExists(globals.FollowIndex).Do(ctx)
+		if err != nil {
+			log.SetFlags(log.LstdFlags | log.Lshortfile)
+			log.Println(err)
+		}
+		// If the index doesn't exist, create it and return error.
+		if !exists {
+			createIndex, Err := eclient.CreateIndex(globals.FollowIndex).BodyString(globals.MappingFollow).Do(ctx)
+			if Err != nil {
+				_, _ = eclient.IndexExists(globals.FollowIndex).Do(ctx)
+				panic(Err)
+			}
+			// TODO fix this.
+			if !createIndex.Acknowledged {
+			}
+
+			// Return an error saying it doesn't exist
+			if err != nil {
+				log.SetFlags(log.LstdFlags | log.Lshortfile)
+				log.Println(err)
+			}
+		}
+
+		var newFollowing = make(map[string]bool)
+		var newFollowers = make(map[string]bool)
+		var newBell = make(map[string]bool)
+		var newFollow = types.Follow{
+			DocID: userID,
+
+			UserFollowers:    newFollowers,
+			UserFollowing:    newFollowing,
+			ProjectFollowers: newFollowers,
+			ProjectFollowing: newFollowing,
+			EventFollowers:   newFollowers,
+			EventFollowing:   newFollowing,
+			UserBell:         newBell,
+			ProjectBell:      newBell,
+			EventBell:        newBell,
+		}
+		// Index the document.
+		_, Err := eclient.Index().
+			Index(globals.FollowIndex).
+			Type(globals.FollowType).
+			BodyJson(newFollow).
+			Do(ctx)
+		if Err != nil {
+			log.SetFlags(log.LstdFlags | log.Lshortfile)
+			log.Println(Err)
+		}
 	}
 
 	for _, hit := range searchResult.Hits.Hits {
