22cf22ce1844ef263fc3c2797d3974d338cf5a41
gpDA <geonpy@likelion.org>
2018-08-23 16:06:04 -0400

    street form

--- uses/changeEventLocation.go
+++ uses/changeEventLocation.go
@@ -9,11 +9,12 @@ import (
 //ChangeEventLocation ... CHANGES EVENT THE EVENT'S LISTED LOCATION
 //Requires the atarget event's docID all aspects of a types.LocStruct
 //Returns an error if there was a problem with database submission
-func ChangeEventLocation(eclient *elastic.Client, eventID string, country string, state string, city string, zip string) error {
+func ChangeEventLocation(eclient *elastic.Client, eventID string, country string, state string, city string, street string, zip string) error {
 	var newLoc types.LocStruct
 	newLoc.Country = country
 	newLoc.State = state
 	newLoc.City = city
+	newLoc.Street = street
 	newLoc.Zip = zip
 
 	err := post.UpdateEvent(eclient, eventID, "Location", newLoc)
