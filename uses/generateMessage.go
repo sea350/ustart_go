8b3cb38d5ecdbcfbe6ae0d0da99d43a5e67430f0
sea350 <sea350@nyu.edu>
2018-08-23 14:08:12 -0400

    progress

--- uses/generateMessage.go
+++ uses/generateMessage.go
@@ -8,22 +8,31 @@ import (
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
-//GenerateMessage ...
-func GenerateMessage(eclient *elastic.Client, notif types.Notification) (string, error) {
+//GenerateNotifMsgAndLink ... creates the text representation of a notification
+// Return message, link, and error
+func GenerateNotifMsgAndLink(eclient *elastic.Client, notif types.Notification) (string, string, error) {
 	switch notif.Class {
 	case 4:
 		if len(notif.ReferenceIDs) > 1 {
-			return strconv.Itoa(len(notif.ReferenceIDs)) + " new followers", nil
+			return "You have " + strconv.Itoa(len(notif.ReferenceIDs)) + " new followers", `/404`, nil
 		} else if len(notif.ReferenceIDs) == 1 {
 			head, err := ConvertUserToFloatingHead(eclient, notif.ReferenceIDs[0])
 			if err != nil {
-				return ``, err
+				return ``, ``, err
 			}
-			return head.FirstName + ` ` + head.LastName + ` is now following you`, nil
+			return head.FirstName + ` ` + head.LastName + ` is now following you`, `/profile/` + head.Username, nil
 		} else {
-			return ``, errors.New("invalid notification")
+			return ``, ``, errors.New("invalid notification")
 		}
+	case 12:
+		head, err := ConvertProjectToFloatingHead(eclient, notif.RedirectToID)
+		if err != nil {
+			return ``, ``, err
+		}
+		return head.FirstName + ` has accepted your request to join!`, `/Projects/` + head.Username, nil
+	case 13:
+
 	}
 
-	return ``, errors.New("invalid notif")
+	return "", ``, errors.New("invalid notif")
 }
