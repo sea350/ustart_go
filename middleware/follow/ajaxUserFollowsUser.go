ae3e4da867c3518679cbb7163bc583e77b640eb1
mrb721 <mrb_2059@yahoo.com>
2018-08-24 13:19:32 -0400

    debug

--- middleware/follow/ajaxUserFollowsUser.go
+++ middleware/follow/ajaxUserFollowsUser.go
@@ -24,7 +24,7 @@ func AjaxUserFollowsUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	followingID := r.FormValue("userID")
-	followingID := r.FormValue("userID")
+
 	fmt.Println("THE FOLLOWING ID:", followingID)
 	fmt.Println("THE USER ID:", followingID)
 	isFollowing, err := getFollow.IsFollowing(client.Eclient, ID.(string), followingID, "user")
