916e81e97edb61d71207cf17053caeafab2912ee
mrb721 <mrb_2059@yahoo.com>
2018-08-25 19:15:09 -0400

    ruf

--- post/follow/removeUserFollow.go
+++ post/follow/removeUserFollow.go
@@ -51,20 +51,20 @@ func RemoveUserFollow(eclient *elastic.Client, userID string, field string, dele
 		return errors.New("Invalid field")
 	}
 
-	// var theField string
-	// if strings.ToLower(field) == "followers" {
-	// 	theField = "UserFollowers"
-	// } else if strings.ToLower(field) == "following" {
-	// 	theField = "UserFollowing"
-	// }
-	// fmt.Println("THE FIELD:", theField)
-	// fmt.Println("FOLLOW MAP:", followMap)
+	var theField string
+	if strings.ToLower(field) == "followers" {
+		theField = "UserFollowers"
+	} else if strings.ToLower(field) == "following" {
+		theField = "UserFollowing"
+	}
+	fmt.Println("THE FIELD:", theField)
+	fmt.Println("FOLLOW MAP:", followMap)
 
 	_, err = eclient.Update().
 		Index(globals.FollowIndex).
 		Type(globals.FollowType).
 		Id(follID).
-		Doc(map[string]interface{}{field: followMap}). //field = Followers or Following, newContent =
+		Doc(map[string]interface{}{theField: followMap}). //field = Followers or Following, newContent =
 		Do(ctx)
 
 	return err
