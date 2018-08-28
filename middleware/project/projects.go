967d3cc0e3546b17f9b2a8a06bc6f4b06e276b4a
sea350 <sea350@nyu.edu>
2018-08-28 18:36:53 -0400

    retest

--- middleware/project/projects.go
+++ middleware/project/projects.go
@@ -18,31 +18,21 @@ import (
 //ProjectsPage ... Displays the projects page
 func ProjectsPage(w http.ResponseWriter, r *http.Request) {
 	session, _ := client.Store.Get(r, "session_please")
-	test1, _ := session.Values["DocID"]
-	if test1 == nil {
+	docID, _ := session.Values[`DocID`]
+	if docID == nil {
 		http.Redirect(w, r, "/~", http.StatusFound)
 		return
 	}
 
-	projID := r.FormValue("projectID")
-
-	_, follDoc, err := getFollow.ByID(client.Eclient, projID)
-
-	if err != nil {
-		log.SetFlags(log.LstdFlags | log.Lshortfile)
-		log.Println(err)
-	}
-
 	var cs client.ClientSide
 
-	_, followingState := follDoc.UserFollowers[session.Values["DocID"].(string)]
-
 	url := r.URL.Path[10:]
-	fmt.Println("THE PROJECT URL", url)
-	fmt.Println("ALL FOLLOWERS:", follDoc.UserFollowers)
-	project, err := uses.AggregateProjectData(client.Eclient, r.URL.Path[10:], test1.(string))
+	if url == `` {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(`NO URL PASSED`)
+	}
 
-	fmt.Println("FollowingState:", followingState)
+	project, err := uses.AggregateProjectData(client.Eclient, r.URL.Path[10:], docID.(string))
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		log.Println(err)
@@ -62,7 +52,7 @@ func ProjectsPage(w http.ResponseWriter, r *http.Request) {
 			log.Println(eror)
 		}
 	}
-	userstruct, err := get.UserByID(client.Eclient, session.Values["DocID"].(string))
+	userstruct, err := get.UserByID(client.Eclient, docID.(string))
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		log.Println(err)
@@ -70,9 +60,15 @@ func ProjectsPage(w http.ResponseWriter, r *http.Request) {
 		cs.ErrorOutput = err
 	}
 
+	_, follDoc, err := getFollow.ByID(client.Eclient, project.DocID)
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+	}
+	_, followingState := follDoc.UserFollowers[docID.(string)]
 	numberFollowers := len(follDoc.UserFollowers)
 	// numberFollowing := len(follDoc.UserFollowing) + len(follDoc.ProjectFollowing) + len(follDoc.EventFollowing)
-	cs = client.ClientSide{UserInfo: userstruct, DOCID: session.Values["DocID"].(string), Username: session.Values["Username"].(string), Followers: int(numberFollowers), FollowingStatus: followingState, Project: project, Widgets: widgets}
+	cs = client.ClientSide{UserInfo: userstruct, DOCID: docID.(string), Username: session.Values["Username"].(string), Followers: int(numberFollowers), FollowingStatus: followingState, Project: project, Widgets: widgets}
 
 	client.RenderSidebar(w, r, "template2-nil")
 	client.RenderSidebar(w, r, "leftnav-nil")
@@ -82,15 +78,15 @@ func ProjectsPage(w http.ResponseWriter, r *http.Request) {
 //MyProjects ... ManageProject
 func MyProjects(w http.ResponseWriter, r *http.Request) {
 	session, _ := client.Store.Get(r, "session_please")
-	test1, _ := session.Values["DocID"]
-	if test1 == nil {
+	docID, _ := session.Values[`DocID`]
+	if docID == nil {
 		http.Redirect(w, r, "/~", http.StatusFound)
 		return
 	}
 
 	var heads []types.FloatingHead
 	var cs client.ClientSide
-	userstruct, err := get.UserByID(client.Eclient, session.Values["DocID"].(string))
+	userstruct, err := get.UserByID(client.Eclient, docID.(string))
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		log.Println(err)
@@ -109,7 +105,7 @@ func MyProjects(w http.ResponseWriter, r *http.Request) {
 		}
 		heads = append(heads, head)
 	}
-	cs = client.ClientSide{UserInfo: userstruct, DOCID: session.Values["DocID"].(string), Username: session.Values["Username"].(string), ListOfHeads: heads}
+	cs = client.ClientSide{UserInfo: userstruct, DOCID: docID.(string), Username: session.Values["Username"].(string), ListOfHeads: heads}
 	client.RenderSidebar(w, r, "template2-nil")
 	client.RenderSidebar(w, r, "leftnav-nil")
 	client.RenderTemplate(w, r, "manageprojects-Nil", cs)
@@ -118,17 +114,17 @@ func MyProjects(w http.ResponseWriter, r *http.Request) {
 //CreateProjectPage ...
 func CreateProjectPage(w http.ResponseWriter, r *http.Request) {
 	session, _ := client.Store.Get(r, "session_please")
-	test1, _ := session.Values["DocID"]
-	if test1 == nil {
+	docID, _ := session.Values[`DocID`]
+	if docID == nil {
 		http.Redirect(w, r, "/~", http.StatusFound)
 		return
 	}
-	userstruct, err := get.UserByID(client.Eclient, session.Values["DocID"].(string))
+	userstruct, err := get.UserByID(client.Eclient, docID.(string))
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		log.Println(err)
 	}
-	cs := client.ClientSide{UserInfo: userstruct, DOCID: session.Values["DocID"].(string), Username: session.Values["Username"].(string)}
+	cs := client.ClientSide{UserInfo: userstruct, DOCID: docID.(string), Username: session.Values["Username"].(string)}
 
 	title := r.FormValue("project_title")
 	description := []rune(r.FormValue("project_desc"))
@@ -163,7 +159,7 @@ func CreateProjectPage(w http.ResponseWriter, r *http.Request) {
 			return
 
 		}
-		url, err := uses.CreateProject(client.Eclient, title, description, session.Values["DocID"].(string), category, college, customURL, projLocation)
+		url, err := uses.CreateProject(client.Eclient, title, description, docID.(string), category, college, customURL, projLocation)
 		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
 			log.Println(err)
