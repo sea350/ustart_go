333c4032bf9d36c1b83d8d9e3f37e2a873c61071
Gitnil <garudyne@gmail.com>
2018-08-30 15:12:48 -0400

    Emember Suggestion?

--- middleware/event/findEventMember.go
+++ middleware/event/findEventMember.go
@@ -7,21 +7,24 @@ import (
 
 //FindEventMember ... find event members
 func FindEventMember(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("FindEventMember Start")
 	fmt.Fprintln(w, "Magic Johnson (Member)")
-	fmt.Println("FindEventMember End")
+	fmt.Fprintln(w, "Magic King (Member)")
+	fmt.Fprintln(w, "Magic Queen (Member)")
+	fmt.Fprintln(w, "Magic Rook (Member)")
 }
 
 //FindEventGuest ... find event guest
 func FindEventGuest(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("FindEventGuest Start")
 	fmt.Fprintln(w, "Magic Johnson (Guest)")
-	fmt.Println("FindEventGuest End")
+	fmt.Fprintln(w, "Magic Pawn (Guest)")
+	fmt.Fprintln(w, "Magic Octopus (Guest)")
+	fmt.Fprintln(w, "Magic Histu (Guest)")
 }
 
 //FindEventProject ... find event project
 func FindEventProject(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("FindEventProject Start")
 	fmt.Fprintln(w, "Magic Johnson (Project)")
-	fmt.Println("FindEventProject End")
+	fmt.Fprintln(w, "Magic Michael (Project)")
+	fmt.Fprintln(w, "Magic Pencil (Project)")
+	fmt.Fprintln(w, "Magic Jason (Project)")
 }
