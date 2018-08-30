a94c683415c54827706fd4b2886b9e91d9b97213
Gitnil <garudyne@gmail.com>
2018-08-30 14:58:34 -0400

    Debug :<|

--- middleware/event/findEventMember.go
+++ middleware/event/findEventMember.go
@@ -7,15 +7,21 @@ import (
 
 //FindEventMember ... find event members
 func FindEventMember(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("FindEventMember Start")
 	fmt.Fprintln(w, "Magic Johnson (Member)")
+	fmt.Println("FindEventMember End")
 }
 
 //FindEventGuest ... find event guest
 func FindEventGuest(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("FindEventGuest Start")
 	fmt.Fprintln(w, "Magic Johnson (Guest)")
+	fmt.Println("FindEventGuest End")
 }
 
 //FindEventProject ... find event project
 func FindEventProject(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("FindEventProject Start")
 	fmt.Fprintln(w, "Magic Johnson (Project)")
+	fmt.Println("FindEventProject End")
 }
