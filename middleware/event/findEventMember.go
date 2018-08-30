35077dd5aaa196d92811d5bff239f306b52934f0
Gitnil <garudyne@gmail.com>
2018-08-30 15:18:13 -0400

    Array Json?

--- middleware/event/findEventMember.go
+++ middleware/event/findEventMember.go
@@ -1,30 +1,28 @@
 package event
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 //FindEventMember ... find event members
 func FindEventMember(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Magic Johnson (Member)")
-	fmt.Fprintln(w, "Magic King (Member)")
-	fmt.Fprintln(w, "Magic Queen (Member)")
-	fmt.Fprintln(w, "Magic Rook (Member)")
+	jsonthis := []string{"Magic Johnson, Magic White, Magic Pomper"}
+	jsonnow, _ := json.Marshal(jsonthis)
+	fmt.Fprintln(w, jsonnow)
 }
 
 //FindEventGuest ... find event guest
 func FindEventGuest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Magic Johnson (Guest)")
-	fmt.Fprintln(w, "Magic Pawn (Guest)")
-	fmt.Fprintln(w, "Magic Octopus (Guest)")
-	fmt.Fprintln(w, "Magic Histu (Guest)")
+	jsonthis := []string{"Magic Johnson, Magic White, Magic Pomper"}
+	jsonnow, _ := json.Marshal(jsonthis)
+	fmt.Fprintln(w, jsonnow)
 }
 
 //FindEventProject ... find event project
 func FindEventProject(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Magic Johnson (Project)")
-	fmt.Fprintln(w, "Magic Michael (Project)")
-	fmt.Fprintln(w, "Magic Pencil (Project)")
-	fmt.Fprintln(w, "Magic Jason (Project)")
+	jsonthis := []string{"Magic Johnson, Magic White, Magic Pomper"}
+	jsonnow, _ := json.Marshal(jsonthis)
+	fmt.Fprintln(w, jsonnow)
 }
