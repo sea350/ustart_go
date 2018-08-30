62e5d357aa3538360ad2d53a457991652a5a12cb
Gitnil <garudyne@gmail.com>
2018-08-30 16:15:23 -0400

    Term?

--- middleware/event/findEventMember.go
+++ middleware/event/findEventMember.go
@@ -7,21 +7,24 @@ import (
 
 //FindEventMember ... find event members
 func FindEventMember(w http.ResponseWriter, r *http.Request) {
-	jsonthis := []string{"Magic Johnson", "Magic White", "Magic Pomper"}
+	term := r.FormValue("term")
+	jsonthis := []string{"Magic Johnson", "Magic White", "Magic Pomper", term}
 	jsonnow, _ := json.Marshal(jsonthis)
 	w.Write(jsonnow)
 }
 
 //FindEventGuest ... find event guest
 func FindEventGuest(w http.ResponseWriter, r *http.Request) {
-	jsonthis := []string{"Magic Johnson", "Magic White", "Magic Pomper"}
+	term := r.FormValue("term")
+	jsonthis := []string{"Magic Johnson", "Magic White", "Magic Pomper", term}
 	jsonnow, _ := json.Marshal(jsonthis)
 	w.Write(jsonnow)
 }
 
 //FindEventProject ... find event project
 func FindEventProject(w http.ResponseWriter, r *http.Request) {
-	jsonthis := []string{"Magic Johnson", "Magic White", "Magic Pomper"}
+	term := r.FormValue("term")
+	jsonthis := []string{"Magic Johnson", "Magic White", "Magic Pomper", term}
 	jsonnow, _ := json.Marshal(jsonthis)
 	w.Write(jsonnow)
 }
