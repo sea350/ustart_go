99d4716ca95bfb40afd005946228e332c0a18972
mrb721 <mrb_2059@yahoo.com>
2018-09-04 14:36:40 -0400

    project tags cleaned

--- middleware/project/updateTags.go
+++ middleware/project/updateTags.go
@@ -24,15 +24,20 @@ func UpdateTags(w http.ResponseWriter, r *http.Request) {
 	p := bluemonday.UGCPolicy()
 
 	ID := r.FormValue("projectWidget")
-	cleanTags := p.Sanitize(ID)
 
-	tags := strings.Split(cleanTags, `","`)
+	tags := strings.Split(ID, `","`)
+
 	if len(tags) > 0 {
 		tags[0] = strings.Trim(tags[0], `["`)
 		tags[len(tags)-1] = strings.Trim(tags[len(tags)-1], `"]`)
 	}
 
-	err := post.UpdateProject(client.Eclient, ID, "Tags", tags)
+	var cleanTags []string
+	for idx := range tags {
+		cleanTags = append(cleanTags, p.Sanitize(tags[idx]))
+	}
+
+	err := post.UpdateProject(client.Eclient, ID, "Tags", cleanTags)
 	if err != nil {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 		dir, _ := os.Getwd()
