9ec6f39d1e081fdf96f4ce6a38670ebc27132fa3
mrb721 <mrb_2059@yahoo.com>
2018-09-04 15:03:02 -0400

    nvm escape

--- middleware/profile/addTag.go
+++ middleware/profile/addTag.go
@@ -2,6 +2,7 @@ package profile
 
 import (
 	"encoding/json"
+	"html"
 	"log"
 	"net/http"
 	"os"
@@ -38,7 +39,7 @@ func AddTag(w http.ResponseWriter, r *http.Request) {
 
 	for t := range ts.Tags {
 		ts.Tags[t] = p.Sanitize(ts.Tags[t])
-		// ts.Tags[t] = html.EscapeString(ts.Tags[t])
+		ts.Tags[t] = html.EscapeString(ts.Tags[t])
 	}
 	// tags := strings.Split(r.FormValue("skillArray"), `","`)
 	// var sanitizedTags []string
