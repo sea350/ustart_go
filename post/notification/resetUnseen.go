bf56a79929ab9ab84b65bc035caf8d755e4aa586
sea350 <sea350@nyu.edu>
2018-09-06 17:06:08 -0400

    lets just try this

--- post/notification/resetUnseen.go
+++ post/notification/resetUnseen.go
@@ -1,6 +1,8 @@
 package post
 
 import (
+	"strings"
+
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
@@ -11,7 +13,7 @@ func ResetUnseen(eclient *elastic.Client, proxyID string) error {
 	ModifyUnseen.Lock()
 	defer ModifyUnseen.Unlock()
 
-	err := UpdateProxyNotifcations(eclient, proxyID, "NumUnseen", 0)
+	err := UpdateProxyNotifcations(eclient, strings.ToLower(proxyID), "NumUnseen", 0)
 
 	return err
 }
