0f883999f5916606ba9758364d5f4b40ada1cd43
mrb721 <mrb_2059@yahoo.com>
2018-09-07 13:29:46 -0400

    search

--- search/prototypeProjectSearchScroll.go
+++ search/prototypeProjectSearchScroll.go
@@ -81,13 +81,20 @@ func PrototypeProjectSearchScroll(eclient *elastic.Client, searchTerm string, so
 	}
 	// Tag
 	if len(mustTag) > 0 {
-		for _, element := range mustTag {
-			//Check if NewMatchQuery order is correct
-			query = query.Must(elastic.NewMatchQuery("Tags", element))
-			for _, element := range stringArray {
-				query = query.Should(elastic.NewFuzzyQuery("Tags", strings.ToLower(element)).Fuzziness(1))
-			}
+		tags := make([]interface{}, 0)
+		for tag := range mustTag {
+			tags = append([]interface{}{strings.ToLower(mustTag[tag])}, tags...)
 		}
+
+		query = query.Must(elastic.NewTermsQuery("Tags", tags...))
+		// for _, element := range mustTag {
+
+		// 	//Check if NewMatchQuery order is correct
+		// 	query = query.Must(elastic.NewMatchQuery("Tags", element))
+		// 	for _, element := range stringArray {
+		// 		query = query.Should(elastic.NewFuzzyQuery("Tags", strings.ToLower(element)).Fuzziness(1))
+		// 	}
+		// }
 	}
 
 	scroll := eclient.Scroll().
