7a7eb8efbd549cde7ad71a653d246e74f9ce543d
sea350 <sea350@nyu.edu>
2018-09-20 09:20:50 -0400

    not

--- middleware/profile/ajaxLoadSuggestedUsers.go
+++ middleware/profile/ajaxLoadSuggestedUsers.go
@@ -1,9 +1,15 @@
 package profile
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
+	getFollow "github.com/sea350/ustart_go/get/follow"
+	get "github.com/sea350/ustart_go/get/user"
 	client "github.com/sea350/ustart_go/middleware/client"
+	properloading "github.com/sea350/ustart_go/properloading"
 )
 
 //AjaxLoadSuggestedUsers ... pulls suggested users based on user's projects and shared skills
@@ -15,36 +21,34 @@ func AjaxLoadSuggestedUsers(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/~", http.StatusFound)
 		return
 	}
-	/*
-		ID := session.Values["DocID"].(string)
-		scrollID := r.FormValue("scrollID")
+	ID := session.Values["DocID"].(string)
+	scrollID := r.FormValue("scrollID")
 
-		myUser, err := get.UserByID(client.Eclient, ID)
-		if err != nil {
-			log.SetFlags(log.LstdFlags | log.Lshortfile)
-			log.Println(err)
-		}
+	myUser, err := get.UserByID(client.Eclient, ID)
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+	}
 
-		_, follDoc, err := getFollow.ByID(client.Eclient, ID)
+	_, follDoc, err := getFollow.ByID(client.Eclient, ID)
 
-		sID, heads, hits, err := properloading.ScrollSuggestedUsers(client.Eclient, myUser.Tags, myUser.Projects, follDoc.UserFollowing, ID, scrollID)
+	sID, heads, hits, err := properloading.ScrollSuggestedUsers(client.Eclient, myUser.Tags, myUser.Projects, follDoc.UserFollowing, ID, scrollID)
 
-		if err != nil {
-			log.SetFlags(log.LstdFlags | log.Lshortfile)
-			log.Println(err)
-		}
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+	}
 
-		var results = make(map[string]interface{})
-		results["scrollID"] = sID
-		results["SuggestedUsers"] = heads
-		results["TotalHits"] = hits
+	var results = make(map[string]interface{})
+	results["scrollID"] = sID
+	results["SuggestedUsers"] = heads
+	results["TotalHits"] = hits
 
-		data, err := json.Marshal(results)
-		if err != nil {
-			log.SetFlags(log.LstdFlags | log.Lshortfile)
-			log.Println(err)
-		}
+	data, err := json.Marshal(results)
+	if err != nil {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println(err)
+	}
 
-		fmt.Fprintln(w, string(data))
-	*/
+	fmt.Fprintln(w, string(data))
 }
