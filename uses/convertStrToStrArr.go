e46b43f9d519d8dec6e2145bc5a8d270bcb862f3
sea350 <sea350@nyu.edu>
2018-10-02 15:35:35 -0400

    i dindu nuffin

--- uses/convertStrToStrArr.go
+++ uses/convertStrToStrArr.go
@@ -1,26 +1,12 @@
 package uses
 
-import "strings"
+import (
+	"encoding/json"
+)
 
 //ConvertStrToStrArr ... given a json format array as a string type, returns string array
 func ConvertStrToStrArr(unbrokenString string) []string {
 	var stringArray []string
-	stringArray = strings.Split(unbrokenString, `","`)
-	if len(stringArray) > 0 {
-		/*
-			//checks if input string in right format
-			if len(stringArray[0]) > 2 {
-				if stringArray[0][0:1] != `["` || stringArray[len(stringArray)-1][len(stringArray[len(stringArray)-1])-2:len(stringArray[len(stringArray)-1])-1] != `"]` {
-					return []string{}
-				}
-			}
-		*/
-		stringArray[0] = strings.Trim(stringArray[0], `["`)
-		stringArray[len(stringArray)-1] = strings.Trim(stringArray[len(stringArray)-1], `"]`)
-		if stringArray[0] == `` && len(stringArray) == 1 {
-			var empty []string
-			stringArray = empty
-		}
-	}
+	_ = json.Unmarshal([]byte(unbrokenString), &stringArray)
 	return stringArray
 }
