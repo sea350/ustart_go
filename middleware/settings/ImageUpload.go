5d36f3abae9f5a675f32a3f9c5dfd188e2024a95
unknown <Jonchow77@gmail.com>
2018-10-04 12:26:58 -0400

    Image upload is fixed

--- middleware/settings/ImageUpload.go
+++ middleware/settings/ImageUpload.go
@@ -15,7 +15,7 @@ func ImageUpload(w http.ResponseWriter, r *http.Request) {
 	test1, _ := session.Values["DocID"]
 	if test1 == nil {
 		fmt.Println(test1)
-		http.Redirect(w, r, "/~", http.StatusFound)
+		http.Redirect(w, r, "/Settings/", http.StatusFound)
 		return
 	}
 	r.ParseForm()
@@ -24,25 +24,25 @@ func ImageUpload(w http.ResponseWriter, r *http.Request) {
 	blob := r.FormValue("image-data")
 	if err != nil {
 		fmt.Println("err: middleware/settings/imageupload line 23\n", err)
+		http.Redirect(w, r, "/~", http.StatusFound)
 		return
-	} else {
-		//Checking if image is valid by checking the first 512 bytes for correct image signature
-		buffer := make([]byte, 512)
-		_, _ = clientFile.Read(buffer)
-		defer clientFile.Close()
-		if http.DetectContentType(buffer)[0:5] == "image" || header.Size == 0 {
-			err = uses.ChangeAccountImagesAndStatus(client.Eclient, session.Values["DocID"].(string), blob, true, ``, "Avatar")
-			if err != nil {
-				log.SetFlags(log.LstdFlags | log.Lshortfile)
-				log.Println(err)
-			} else {
-				session.Values["Avatar"] = blob
-				session.Save(r, w)
-			}
-		} else {
+	}
+	//Checking if image is valid by checking the first 512 bytes for correct image signature
+	buffer := make([]byte, 512)
+	_, _ = clientFile.Read(buffer)
+	defer clientFile.Close()
+	if http.DetectContentType(buffer)[0:5] == "image" || header.Size == 0 {
+		err = uses.ChangeAccountImagesAndStatus(client.Eclient, session.Values["DocID"].(string), blob, true, ``, "Avatar")
+		if err != nil {
 			log.SetFlags(log.LstdFlags | log.Lshortfile)
-			log.Println("invalid file upload")
+			log.Println(err)
+		} else {
+			session.Values["Avatar"] = blob
+			session.Save(r, w)
 		}
+	} else {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.Println("invalid file upload")
 	}
 
 	http.Redirect(w, r, "/Settings/#avatarcollapse", http.StatusFound)
