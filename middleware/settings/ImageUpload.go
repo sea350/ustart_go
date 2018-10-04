139ebbe834e7f1052633ba9f00ebe96eade04040
unknown <Jonchow77@gmail.com>
2018-10-04 12:19:06 -0400

    Fixed image upload

--- middleware/settings/ImageUpload.go
+++ middleware/settings/ImageUpload.go
@@ -25,24 +25,24 @@ func ImageUpload(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("err: middleware/settings/imageupload line 23\n", err)
 		return
-	}
-
-	//Checking if image is valid by checking the first 512 bytes for correct image signature
-	buffer := make([]byte, 512)
-	_, _ = clientFile.Read(buffer)
-	defer clientFile.Close()
-	if http.DetectContentType(buffer)[0:5] == "image" || header.Size == 0 {
-		err = uses.ChangeAccountImagesAndStatus(client.Eclient, session.Values["DocID"].(string), blob, true, ``, "Avatar")
-		if err != nil {
-			log.SetFlags(log.LstdFlags | log.Lshortfile)
-			log.Println(err)
-		} else {
-			session.Values["Avatar"] = blob
-			session.Save(r, w)
-		}
 	} else {
-		log.SetFlags(log.LstdFlags | log.Lshortfile)
-		log.Println("invalid file upload")
+		//Checking if image is valid by checking the first 512 bytes for correct image signature
+		buffer := make([]byte, 512)
+		_, _ = clientFile.Read(buffer)
+		defer clientFile.Close()
+		if http.DetectContentType(buffer)[0:5] == "image" || header.Size == 0 {
+			err = uses.ChangeAccountImagesAndStatus(client.Eclient, session.Values["DocID"].(string), blob, true, ``, "Avatar")
+			if err != nil {
+				log.SetFlags(log.LstdFlags | log.Lshortfile)
+				log.Println(err)
+			} else {
+				session.Values["Avatar"] = blob
+				session.Save(r, w)
+			}
+		} else {
+			log.SetFlags(log.LstdFlags | log.Lshortfile)
+			log.Println("invalid file upload")
+		}
 	}
 
 	http.Redirect(w, r, "/Settings/#avatarcollapse", http.StatusFound)
