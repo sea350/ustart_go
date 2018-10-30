b3b76e20efd76e9457b56cf93b4e0caaaf44b178
sea350 <sea350@nyu.edu>
2018-10-30 17:28:45 -0400

    should work

--- uses/validGuestCode.go
+++ uses/validGuestCode.go
@@ -3,6 +3,7 @@ package uses
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	getCode "github.com/sea350/ustart_go/get/guestCode"
 	elastic "gopkg.in/olivere/elastic.v5"
@@ -16,10 +17,10 @@ func ValidGuestCode(eclient *elastic.Client, guestCode string) (bool, error) {
 	}
 
 	//Check if code expired (time and number of uses)
-	/*
-		if codeObj.Expiration.Before(time.Now()) {
-			return false, errors.New("Code Expired")
-		}*/
+
+	if codeObj.Expiration.Before(time.Now()) {
+		return false, errors.New("Code Expired")
+	}
 	if codeObj.NumUses-len(codeObj.Users) < 0 {
 		fmt.Println(codeObj.NumUses - len(codeObj.Users))
 		return false, errors.New("Exceeded number of uses")
