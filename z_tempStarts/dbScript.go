be8da76e897b0adb77057998bf92a975884a20d0
mrb721 <mrb_2059@yahoo.com>
2018-12-26 14:10:36 -0500

    debug

--- z_tempStarts/dbScript.go
+++ z_tempStarts/dbScript.go
@@ -472,7 +472,7 @@ func main() {
 			commands = append(commands, input)
 			switch input {
 			case "wipe":
-				indices = append(indices, globals.UserIndex, globals.ProjectIndex, globals.EntryIndex, globals.ConvoIndex, globals.ProxyMsgIndex, globals.MsgIndex, globals.GuestCodeIndex, globals.NotificatonIndex, globals.WidgetIndex, globals.FollowIndex, globals.ImgIndex, globals.EventIndex)
+				indices = append(indices, globals.UserIndex, globals.ProjectIndex, globals.EntryIndex, globals.ConvoIndex, globals.ProxyMsgIndex, globals.MsgIndex, globals.GuestCodeIndex, globals.NotificationIndex, globals.ProxyNotifIndex,, globals.WidgetIndex, globals.FollowIndex, globals.ImgIndex, globals.EventIndex)
 				// delete phase
 				for _, index := range indices {
 					deleteIndex(eclient, index)
@@ -483,12 +483,12 @@ func main() {
 					startIndex(eclient, index)
 				}
 			case "delete":
-				indices = append(indices, globals.UserIndex, globals.ProjectIndex, globals.EntryIndex, globals.ConvoIndex, globals.ProxyMsgIndex, globals.MsgIndex, globals.GuestCodeIndex, globals.NotificatonIndex, globals.WidgetIndex, globals.FollowIndex, globals.ImgIndex, globals.EventIndex)
+				indices = append(indices, globals.UserIndex, globals.ProjectIndex, globals.EntryIndex, globals.ConvoIndex, globals.ProxyMsgIndex, globals.MsgIndex, globals.GuestCodeIndex, globals.NotificationIndex, globals.ProxyNotifIndex,, globals.WidgetIndex, globals.FollowIndex, globals.ImgIndex, globals.EventIndex)
 				for _, index := range indices {
 					deleteIndex(eclient, index)
 				}
 			case "start":
-				indices = append(indices, globals.UserIndex, globals.ProjectIndex, globals.EntryIndex, globals.ConvoIndex, globals.ProxyMsgIndex, globals.MsgIndex, globals.GuestCodeIndex, globals.NotificatonIndex, globals.WidgetIndex, globals.FollowIndex, globals.ImgIndex, globals.EventIndex)
+				indices = append(indices, globals.UserIndex, globals.ProjectIndex, globals.EntryIndex, globals.ConvoIndex, globals.ProxyMsgIndex, globals.MsgIndex, globals.GuestCodeIndex, globals.NotificationIndex, globals.ProxyNotifIndex,, globals.WidgetIndex, globals.FollowIndex, globals.ImgIndex, globals.EventIndex)
 				for _, index := range indices {
 					startIndex(eclient, index)
 				}
@@ -515,7 +515,7 @@ func main() {
 					deleteIndex(eclient, index)
 				}
 			case "delete entries":
-				indices = append(indices, globals.EntriesIndex)
+				indices = append(indices, globals.EntryIndex)
 				for _, index := range indices {
 					deleteIndex(eclient, index)
 				}
@@ -550,15 +550,12 @@ func main() {
 					startIndex(eclient, index)
 				}
 			case "start entries":
-				indices = append(indices, globals.EntriesIndex)
+				indices = append(indices, globals.EntryIndex)
 				for _, index := range indices {
 					startIndex(eclient, index)
 				}
 			case "start chat":
 
-				if proxyErr != nil {
-					log.Println(proxyErr)
-				}
 				indices = append(indices, globals.ConvoIndex, globals.ProxyMsgIndex, globals.MsgIndex)
 				for _, index := range indices {
 					startIndex(eclient, index)
