4f4bf393f35513814ae9b297638d457242fc8e84
NEVETS <sea350@nyu.edu>
2019-01-12 17:23:43 -0500

    ?

--- middleware/profile/GetComments.go
+++ middleware/profile/GetComments.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	
+
 	"net/http"
 
 	client "github.com/sea350/ustart_go/middleware/client"
@@ -23,14 +23,18 @@ func GetComments(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 	postID := r.FormValue("PostID")
+	if postID == `` {
+		client.Logger.Println("DocID: " + session.Values["DocID"].(string) + " | " + "Critical data not passed in")
+	}
+
 	_, arrayofComments, err := uses.LoadComments(client.Eclient, postID, docID.(string), 0, -1)
 	if err != nil && err != errors.New("This entry is not visible") {
-		
 		client.Logger.Println("DocID: "+session.Values["DocID"].(string)+" | err: ", err)
 	}
+
 	data, err := json.Marshal(arrayofComments)
 	if err != nil {
-		
+
 		client.Logger.Println("DocID: "+session.Values["DocID"].(string)+" | err: ", err)
 	}
 	fmt.Fprintln(w, string(data))
