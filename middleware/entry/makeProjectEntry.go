4b4ec3d511891a6374fa289b548d34b1e1df1324
NEVETS <sea350@nyu.edu>
2019-01-26 14:06:21 -0500

    spam lockedout

--- middleware/entry/makeProjectEntry.go
+++ middleware/entry/makeProjectEntry.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"html"
-	
+
+	"github.com/sea350/ustart_go/antispam"
+
 	"net/http"
 
 	get "github.com/sea350/ustart_go/get/project"
@@ -27,12 +29,21 @@ func MakeProjectEntry(w http.ResponseWriter, r *http.Request) {
 	}
 
 	projectID := r.FormValue("docID")
+	if projectID == `` {
+		client.Logger.Println("DocID: " + session.Values["DocID"].(string) + " | " + "Critical data not passed in")
+		return
+	}
 	newContent := html.EscapeString(r.FormValue("text"))
 	proj, member, err := get.ProjAndMember(client.Eclient, projectID, docID.(string))
 
 	if err != nil {
-		
+
 		client.Logger.Println("DocID: "+session.Values["DocID"].(string)+" | err: ", err)
+	}
+
+	if !antispam.AntiJournalSpam(docID.(string)) {
+		client.Logger.Println("DocID: " + session.Values["DocID"].(string) + " | This user is attempting to spam on project " + projectID)
+		return
 	}
 
 	if uses.HasPrivilege("post", proj.PrivilegeProfiles, member) {
@@ -41,24 +52,24 @@ func MakeProjectEntry(w http.ResponseWriter, r *http.Request) {
 
 		entryID, err := postEntry.IndexEntry(client.Eclient, entry)
 		if err != nil {
-			
+
 			client.Logger.Println("DocID: "+session.Values["DocID"].(string)+" | err: ", err)
 		}
 
 		err = post.AppendEntryID(client.Eclient, projectID, entryID)
 		if err != nil {
-			
+
 			client.Logger.Println("DocID: "+session.Values["DocID"].(string)+" | err: ", err)
 		}
 
 		jEntry, err := uses.ConvertEntryToJournalEntry(client.Eclient, entryID, docID.(string), true)
 		if err != nil {
-			
+
 			client.Logger.Println("DocID: "+session.Values["DocID"].(string)+" | err: ", err)
 		}
 		data, err := json.Marshal(jEntry)
 		if err != nil {
-			
+
 			client.Logger.Println("DocID: "+session.Values["DocID"].(string)+" | err: ", err)
 		}
 		fmt.Fprintln(w, string(data))
