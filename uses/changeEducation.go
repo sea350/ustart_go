0d47ba6b0aaca3c66d9a446ea6b79237edbbc10c
NEVETS <sea350@nyu.edu>
2019-01-28 14:03:48 -0500

    lolwut

--- uses/changeEducation.go
+++ uses/changeEducation.go
@@ -1,19 +1,13 @@
 package uses
 
 import (
-	"strings"
-
 	post "github.com/sea350/ustart_go/post/user"
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
 //ChangeEducation ...
-func ChangeEducation(eclient *elastic.Client, userID string, accType int, uni string, uniGrad string, major []string, minor []string, class string) error {
+func ChangeEducation(eclient *elastic.Client, userID string, uni string, uniGrad string, major []string, minor []string, class string) error {
 
-	err := post.UpdateUser(eclient, userID, "AccType", accType)
-	if err != nil {
-		return err
-	}
 	// err = post.UpdateUser(eclient, userID, "HighSchool", hs)
 	// if err != nil {
 	// 	return err
@@ -22,7 +16,7 @@ func ChangeEducation(eclient *elastic.Client, userID string, accType int, uni st
 	// if err != nil {
 	// 	return err
 	// }
-	err = post.UpdateUser(eclient, userID, "UndergradSchool", uni)
+	err := post.UpdateUser(eclient, userID, "UndergradSchool", uni)
 	if err != nil {
 		return err
 	}
@@ -37,21 +31,4 @@ func ChangeEducation(eclient *elastic.Client, userID string, accType int, uni st
 	err = post.UpdateUser(eclient, userID, "Minors", minor)
 	return err
 
-	var classInt int = 0
-	switch strings.ToLower(class) {
-	case "freshman":
-		classInt = 0
-	case "sophomore":
-		classInt = 1
-	case "junior":
-		classInt = 2
-	case "senior":
-		classInt = 3
-	case "graduate":
-		classInt = 4
-	case "alumni":
-		classInt = 5
-	}
-	err = post.UpdateUser(eclient, userID, "Class", classInt)
-	return err
 }
