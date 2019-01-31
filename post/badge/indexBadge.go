410e31be5f6ee1b359b735a398306e482d223f0a
min <mrb_2059@yahoo.com>
2019-01-31 17:05:49 -0500

    debug

--- post/badge/indexBadge.go
+++ post/badge/indexBadge.go
@@ -38,7 +38,7 @@ func IndexBadge(eclient *elastic.Client, newBadge types.Badge) (string, error) {
 	}
 
 	// Index the document.
-	newUsr, Err := eclient.Index().
+	newBadge, Err := eclient.Index().
 		Index(globals.BadgeIndex).
 		Type(globals.BadgeType).
 		BodyJson(newBadge).
