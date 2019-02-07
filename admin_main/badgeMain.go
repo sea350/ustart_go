f65a52793031548a5ac254604a5a5c91ccbbb7de
min <mrb_2059@yahoo.com>
2019-02-07 14:03:25 -0500

    givr badge

---
+++ admin_main/badgeMain.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sea350/ustart_go/admin"
+
+	"github.com/sea350/ustart_go/globals"
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
+
+func main() {
+	err := admin.ModifyBadge(eclient, "USTART", "give", "gl1144@nyu.edu", "")
+	fmt.Println(err)
+}
