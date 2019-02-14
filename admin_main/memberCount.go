d5d70d0ba7fb4bfa22b804a06e25c7b92e9f33e2
min <mrb_2059@yahoo.com>
2019-02-14 14:14:34 -0500

    debug

--- admin_main/memberCount.go
+++ admin_main/memberCount.go
@@ -5,9 +5,11 @@ import (
 	// admin "github.com/sea350/ustart_go/admin"
 
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/sea350/ustart_go/globals"
+	"github.com/sea350/ustart_go/types"
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
@@ -41,10 +43,23 @@ func main() {
 		fmt.Println(err)
 	}
 
+	emails := []string{}
+	for _, id := range res2.Hits.Hits {
+		data := types.User{}
+		err = json.Unmarshal(*id.Source, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
+		emails = append(email, data.Email)
+	}
 	ver := int(res.TotalHits())
 	notVer := int(res2.TotalHits())
 	fmt.Println("Total:", ver+notVer)
 	fmt.Println("Verified:", ver)
 	fmt.Println("Unverified:", notVer)
+	fmt.Println("The following users are unverified: ")
+	for _, e := range emails {
+		fmt.Println(e)
+	}
 
 }
