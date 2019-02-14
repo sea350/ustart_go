39d2fe7c9f8b42a2fcd770a88a4105160939a161
min <mrb_2059@yahoo.com>
2019-02-14 17:53:08 -0500

    ryan project remove

--- admin_main/fixRyan.go
+++ admin_main/fixRyan.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	getFollow "github.com/sea350/ustart_go/get/follow"
 	getUser "github.com/sea350/ustart_go/get/user"
+	post "github.com/sea350/ustart_go/post/user"
 
 	// post "github.com/sea350/ustart_go/post/user"
-	postFollow "github.com/sea350/ustart_go/post/follow"
 
 	// admin "github.com/sea350/ustart_go/admin"
 
@@ -19,21 +18,16 @@ var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
 
 func main() {
 
-	ryanID, err := getUser.IDByUsername(eclient, "ryanrozbiani")
+	usr, err := getUser.UserByUsername(eclient, "ryanrozbiani")
+	usrID, err := getUser.IDByUsername(eclient, "ryanrozbiani")
+
+	fmt.Println(len(updatesProj))
+	updatesProj := append(usr.Projects[:3], usr.Projects[4:]...)
+	err = post.UpdateUser(eclient, usrID, "Projects", updatesProj)
 	if err != nil {
 		fmt.Println("LINE 24,", err)
 	}
 
-	follID, foll, err := getFollow.ByID(eclient, ryanID)
-	if err != nil {
-		fmt.Println("LINE29,", err)
-	}
+	fmt.Println(len(updatesProj))
 
-	var emptyMap = make(map[string]bool)
-	foll.ProjectFollowing = emptyMap
-
-	err = postFollow.ReindexFollow(eclient, follID, foll)
-	if err != nil {
-		fmt.Println("LINE 40,", err)
-	}
 }
