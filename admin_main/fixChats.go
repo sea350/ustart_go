54bc49ea770d61495ffb04db95cc89a639b30ca6
Minhazur Bhuiyan <mrb_2059@yahoo.com>
2019-02-16 22:02:51 -0500

    debug

--- admin_main/fixChats.go
+++ admin_main/fixChats.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	getChat "github.com/sea350/ustart_go/get/chat"
 	globals "github.com/sea350/ustart_go/globals"
-	postChat "github.com/sea350/ustart_go/post/chat"
-	"github.com/sea350/ustart_go/types"
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
@@ -18,9 +15,9 @@ var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
 
 func main() {
 	// ctx := context.Background()
-	proxyID := "g_5h42gBN3VvtvdiWZt3"
+	// proxyID := "g_5h42gBN3VvtvdiWZt3"
 	// usrID, _ := getUser.IDByUsername(eclient, "HeatherMT")
-	// convoID := "9v4r-GgBN3VvtvdieZzG"
+	convoID := "9v4r-GgBN3VvtvdieZzG"
 
 	// fmt.Println("Deleting convo")
 	// _, err := eclient.Delete().
@@ -47,39 +44,51 @@ func main() {
 	// 	BodyJson(theProxy).
 	// 	Do(ctx)
 
-	var blankTime time.Time
-	var convoState = types.ConversationState{
-		// NumUnread   int       `json:"NumUnread"`
-		// LastMessage Message   `json:"LastMessage"`
-		ConvoID:     "-f6_6WgBN3VvtvdiTJtI",
-		ProjectID:   "9_6_6WgBN3VvtvdiTJsk",
-		Read:        true,
-		Muted:       false,
-		MuteTimeout: blankTime,
+	// var blankTime time.Time
+	// var convoState = types.ConversationState{
+	// 	// NumUnread   int       `json:"NumUnread"`
+	// 	// LastMessage Message   `json:"LastMessage"`
+	// 	ConvoID:     "-f6_6WgBN3VvtvdiTJtI",
+	// 	ProjectID:   "9_6_6WgBN3VvtvdiTJsk",
+	// 	Read:        true,
+	// 	Muted:       false,
+	// 	MuteTimeout: blankTime,
+	// }
+
+	// var convoState2 = types.ConversationState{
+	// 	// NumUnread   int       `json:"NumUnread"`
+	// 	// LastMessage Message   `json:"LastMessage"`
+	// 	ConvoID:     "9P4r-GgBN3Vvtvdicpzp",
+	// 	ProjectID:   "",
+	// 	Read:        true,
+	// 	Muted:       false,
+	// 	MuteTimeout: blankTime,
+	// }
+
+	// convoStates := []types.ConversationState{convoState, convoState2}
+
+	// proxyObj, err := getChat.ProxyMsgByID(eclient, proxyID)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// err = postChat.UpdateProxyMsg(eclient, proxyID, "Conversations", append(proxyObj.Conversations, convoStates...))
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	ntq := elastic.NewTermsQuery("_id", convoID)
+
+	res, err := eclient.Search().
+		Index(globals.ConvoIndex).
+		Types(globals.ConvoType).
+		Query(ntq).
+		Do(ctx)
+
+	for _, r := range res.Hits.Hits {
+		cvo, _ := getChat.ConvoByID(eclient, r.Id)
+		fmt.Println(cvo)
 	}
-
-	var convoState2 = types.ConversationState{
-		// NumUnread   int       `json:"NumUnread"`
-		// LastMessage Message   `json:"LastMessage"`
-		ConvoID:     "9P4r-GgBN3Vvtvdicpzp",
-		ProjectID:   "",
-		Read:        true,
-		Muted:       false,
-		MuteTimeout: blankTime,
-	}
-
-	convoStates := []types.ConversationState{convoState, convoState2}
-
-	proxyObj, err := getChat.ProxyMsgByID(eclient, proxyID)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = postChat.UpdateProxyMsg(eclient, proxyID, "Conversations", append(proxyObj.Conversations, convoStates...))
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
 }
