866d65704254b72805a5c32928d5c4135358a384
Minhazur Bhuiyan <mrb_2059@yahoo.com>
2019-02-16 20:30:22 -0500

    debug

--- admin_main/fixChats.go
+++ admin_main/fixChats.go
@@ -3,31 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
-	"strings"
 
-	getChat "github.com/sea350/ustart_go/get/chat"
-	get "github.com/sea350/ustart_go/get/user"
 	globals "github.com/sea350/ustart_go/globals"
-	"github.com/sea350/ustart_go/middleware/client"
-	elastic "gopkg.in/olivere/elastic.v5"
 )
 
 //9v4r-GgBN3VvtvdieZzG
 // g_5h42gBN3VvtvdiWZt3
 
-
 func main() {
 	ctx := context.Background()
-	proxyID := g_5h42gBN3VvtvdiWZt3
-	focusID := 9v4r-GgBN3VvtvdieZzG
-	
+	proxyID := " g_5h42gBN3VvtvdiWZt3"
+	focusID := "9v4r-GgBN3VvtvdieZzG"
+
 	fmt.Println("Deleting convo")
 	err = eclient.Delete().
 		Index(globals.ConvoIndex).
 		Type(globals.ConvoType).
 		Id(focusID).
 		Do(ctx)
-	
+
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -42,7 +36,5 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	
-
 
 }
