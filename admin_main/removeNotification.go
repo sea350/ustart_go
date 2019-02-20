1f3ef1e3d6fa31029a22cde7f56adc522940770c
min <mrb_2059@yahoo.com>
2019-02-20 01:30:10 -0500

    debug

---
+++ admin_main/removeNotification.go
@@ -0,0 +1,57 @@
+//class 4 for follow, refID L, docID R
+package main
+
+import (
+
+	// admin "github.com/sea350/ustart_go/admin"
+
+	"context"
+	"encoding/json"
+	"fmt"
+
+	// postUser "github.com/sea350/ustart_go/post/user"
+	// postNotif "github.com/sea350/ustart_go/post/notification"
+	getUser "github.com/sea350/ustart_go/get/user"
+	// getNotif "github.com/sea350/ustart_go/get/notification"
+	"github.com/sea350/ustart_go/globals"
+	"github.com/sea350/ustart_go/types"
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
+
+func main() {
+
+	ctx := context.Background()
+
+	LiD, _ := getUser.IDByUsername(eclient, "ln961")
+	RiD, _ := getUser.IDByUsername(eclient, "ryanrozbiani")
+
+	maq := elastic.NewBoolQuery()
+	maq = maq.Must(elastic.NewTermQuery("DocID", LiD))
+	maq = maq.Must(elastic.NewTermQuery("ReferenceIDs", RiD))
+	res, err := eclient.Search().
+		Index(globals.NotifIndex).
+		Type(globals.NotifType).
+		Query(maq).
+		Size(100).
+		Do(ctx)
+
+	for _, id := range res.Hits.Hits {
+		data := types.Notification{}
+		err = json.Unmarshal(*id.Source, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if len(data.BadgeIDs) == 0 {
+			fmt.Println(data.Timestamp)
+			//
+			// err = post.UpdateUser(eclient, id.Id, "BadgeIDs", append(data.BadgeIDs, badgeIDs...))
+
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		}
+	}
+}
