b91c371f0630cc9303580474a8b2501d27e3ddcc
min <mrb_2059@yahoo.com>
2019-02-20 01:47:55 -0500

    debug

--- admin_main/removeNotification.go
+++ admin_main/removeNotification.go
@@ -6,15 +6,13 @@ import (
 	// admin "github.com/sea350/ustart_go/admin"
 
 	"context"
-	"encoding/json"
 	"fmt"
 
 	// postUser "github.com/sea350/ustart_go/post/user"
 	// postNotif "github.com/sea350/ustart_go/post/notification"
 	getUser "github.com/sea350/ustart_go/get/user"
-	// getNotif "github.com/sea350/ustart_go/get/notification"
+	// ge/tNotif "github.com/sea350/ustart_go/get/notification"
 	"github.com/sea350/ustart_go/globals"
-	"github.com/sea350/ustart_go/types"
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
@@ -28,32 +26,34 @@ func main() {
 	RiD, _ := getUser.IDByUsername(eclient, "ryanrozbiani")
 
 	fmt.Println("IDs:", LiD, RiD)
-	maq := elastic.NewBoolQuery()
-	maq = maq.Must(elastic.NewTermQuery("DocID", RiD))
-	maq = maq.Must(elastic.NewTermQuery("ReferenceIDs", LiD))
-	res, err := eclient.Search().
-		Index(globals.NotificationIndex).
-		Type(globals.NotificationType).
-		Query(maq).
-		Size(100).
-		Do(ctx)
+	notifs, _ := getNotif.NotificationsByUserID(eclient, LiD)
+	fmt.Println(notifs)
+	// maq := elastic.NewBoolQuery()
+	// maq = maq.Must(elastic.NewTermQuery("DocID", RiD))
+	// maq = maq.Must(elastic.NewTermQuery("ReferenceIDs", LiD))
+	// res, err := eclient.Search().
+	// 	Index(globals.NotificationIndex).
+	// 	Type(globals.NotificationType).
+	// 	Query(maq).
+	// 	Size(100).
+	// 	Do(ctx)
 
-	fmt.Println("nHits:", res.TotalHits())
-	for _, id := range res.Hits.Hits {
-		data := types.Notification{}
-		err = json.Unmarshal(*id.Source, &data)
-		if err != nil {
-			fmt.Println(err)
-		}
+	// fmt.Println("nHits:", res.TotalHits())
+	// for _, id := range res.Hits.Hits {
+	// 	data := types.Notification{}
+	// 	err = json.Unmarshal(*id.Source, &data)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
 
-		fmt.Println("TIMESTAMP:", data.Timestamp)
-		//
-		// err = post.UpdateUser(eclient, id.Id, "BadgeIDs", append(data.BadgeIDs, badgeIDs...))
+	// 	fmt.Println("TIMESTAMP:", data.Timestamp)
+	// 	//
+	// 	// err = post.UpdateUser(eclient, id.Id, "BadgeIDs", append(data.BadgeIDs, badgeIDs...))
 
-		if err != nil {
-			fmt.Println(err)
-			continue
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		continue
 
-		}
-	}
+	// 	}
+	// }
 }
