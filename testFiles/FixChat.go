95760adef60ce60c18826469c7f76a3dfb4aa920
NEVETS <sea350@nyu.edu>
2019-02-23 16:32:18 -0500

    :)

--- testFiles/FixChat.go
+++ testFiles/FixChat.go
@@ -1,76 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"strings"
 
-	getChat "github.com/sea350/ustart_go/get/chat"
-	get "github.com/sea350/ustart_go/get/user"
+	elastic "github.com/olivere/elastic"
+	"github.com/sea350/ustart_go/globals"
+
 	"github.com/sea350/ustart_go/middleware/client"
-	postChat "github.com/sea350/ustart_go/post/chat"
 )
 
 func main() {
-	fmt.Println("getting user by username")
-	// id, err := get.IDByUsername(client.Eclient, "th1750") //
-	id, err := get.IDByUsername(client.Eclient, "support")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("User docID: " + id)
-	proxyid, err := getChat.ProxyIDByUserID(client.Eclient, id)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(proxyid)
-	proxy, err := getChat.ProxyMsgByID(client.Eclient, proxyid)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(proxy)
+	ctx := context.Background()
 
-	var chatFound bool
-	for i := range proxy.Conversations {
-		if proxy.Conversations[i].ConvoID == "9v4r-GgBN3VvtvdieZzG" {
-			proxy.Conversations = append(proxy.Conversations[:i], proxy.Conversations[i+1:]...)
-			fmt.Println(len(proxy.Conversations))
-			fmt.Println(i)
-			chatFound = true
-			break
+	termQuery := elastic.NewTermQuery("DocID", strings.ToLower("v4e02gBN3VvtvdiDZYs"))
+	searchResult, err := client.Eclient.Search().
+		Index(globals.ProxyMsgIndex).
+		Query(termQuery).
+		Do(ctx)
+
+	var proxyID string
+
+	for _, element := range searchResult.Hits.Hits {
+		if "Bf4g02gBN3VvtvdiF5fV" != element.Id {
+			err := globals.DeleteByID(client.Eclient, element.Id, "proxymsg")
+			if err != nil {
+				fmt.Println(element.Id + "failed to be deleted")
+				fmt.Println(err)
+			}
 		}
-	}
-	fmt.Println("chat found status: ", chatFound)
 
-	if chatFound {
-		err = postChat.UpdateProxyMsg(client.Eclient, proxyid, "Conversations", proxy.Conversations)
-		if err != nil {
-			fmt.Println(err)
-		}
 	}
 
-	// query := elastic.NewBoolQuery()
-
-	// query = query.Must(elastic.NewTermQuery("Eavesdroppers.DocID", strings.ToLower(id)))
-
-	// ctx := context.Background() //intialize context background
-	// searchResults, err := client.Eclient.Search().
-	// 	Index(globals.ConvoIndex).
-	// 	Query(query).
-	// 	Pretty(true).
-	// 	Do(ctx)
-
-	// if searchResults.TotalHits() == 0 {
-	// 	fmt.Println("empty")
-	// 	return
-	// }
-	// for _, hit := range searchResults.Hits.Hits {
-	// 	chat, err := getChat.ConvoByID(client.Eclient, hit.Id)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		continue
-	// 	}
-	// 	fmt.Println(hit.Id, chat.ReferenceID, chat.Class, chat.Size)
-	// }
 }
