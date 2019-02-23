cc93e2ea599af938fa55b579f28eac2909bff1a8
NEVETS <sea350@nyu.edu>
2019-02-23 16:36:59 -0500

    hopefully this works

--- get/chat/proxyIDByUserID.go
+++ get/chat/proxyIDByUserID.go
@@ -1,59 +1,56 @@
 package get
 
 import (
-	"context"
-	"errors"
-	"strings"
-
-	globals "github.com/sea350/ustart_go/globals"
-	types "github.com/sea350/ustart_go/types"
 	elastic "github.com/olivere/elastic"
+	getUser "github.com/sea350/ustart_go/get/user"
 )
 
 //ProxyIDByUserID ...
 func ProxyIDByUserID(eclient *elastic.Client, userID string) (string, error) {
 
-	ctx := context.Background()
+	// ctx := context.Background()
 
-	termQuery := elastic.NewTermQuery("DocID", strings.ToLower(userID))
-	searchResult, err := eclient.Search().
-		Index(globals.ProxyMsgIndex).
-		Query(termQuery).
-		Do(ctx)
+	// termQuery := elastic.NewTermQuery("DocID", strings.ToLower(userID))
+	// searchResult, err := eclient.Search().
+	// 	Index(globals.ProxyMsgIndex).
+	// 	Query(termQuery).
+	// 	Do(ctx)
 
-	var proxyID string
-	if err != nil {
-		return proxyID, err
-	}
+	// var proxyID string
+	// if err != nil {
+	// 	return proxyID, err
+	// }
 
-	if searchResult.TotalHits() == 0 {
-		proxy := types.ProxyMessages{DocID: userID, Class: 1}
-		result, err := eclient.Index().
-			Index(globals.ProxyMsgIndex).
-			Type(globals.ProxyMsgType).
-			BodyJson(proxy).
-			Do(ctx)
+	// if searchResult.TotalHits() == 0 {
+	// 	proxy := types.ProxyMessages{DocID: userID, Class: 1}
+	// 	result, err := eclient.Index().
+	// 		Index(globals.ProxyMsgIndex).
+	// 		Type(globals.ProxyMsgType).
+	// 		BodyJson(proxy).
+	// 		Do(ctx)
 
-		return result.Id, err
-	}
-	if searchResult.TotalHits() > 1 {
-		/*
-			for _, element := range searchResult.Hits.Hits {
-				prx, _ := ProxyMsgByID(eclient, element.Id)
-				log.SetFlags(log.LstdFlags | log.Lshortfile)
-				log.Println(prx)
+	// 	return result.Id, err
+	// }
+	// if searchResult.TotalHits() > 1 {
+	// 	/*
+	// 		for _, element := range searchResult.Hits.Hits {
+	// 			prx, _ := ProxyMsgByID(eclient, element.Id)
+	// 			log.SetFlags(log.LstdFlags | log.Lshortfile)
+	// 			log.Println(prx)
 
-			}
-		*/
-		return proxyID, errors.New("multiple proxies found")
-	}
+	// 		}
+	// 	*/
+	// 	return proxyID, errors.New("multiple proxies found")
+	// }
 
-	for _, element := range searchResult.Hits.Hits {
+	// for _, element := range searchResult.Hits.Hits {
 
-		proxyID = element.Id
-		break
-	}
+	// 	proxyID = element.Id
+	// 	break
+	// }
 
-	return proxyID, err
+	usr, err := getUser.UserByID(eclient, userID)
+
+	return usr.ProxyMessagesID, err
 
 }
