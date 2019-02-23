9e5d298286808c594f34e788b7b57f8e08b9e28a
NEVETS <sea350@nyu.edu>
2019-02-23 17:27:34 -0500

    pls work

--- testFiles/fixMinChat.go
+++ testFiles/fixMinChat.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	getChat "github.com/sea350/ustart_go/get/chat"
 	get "github.com/sea350/ustart_go/get/user"
 	"github.com/sea350/ustart_go/middleware/client"
+	postChat "github.com/sea350/ustart_go/post/chat"
+	"github.com/sea350/ustart_go/types"
 )
 
 func main() {
@@ -34,11 +35,21 @@ func main() {
 		return
 	}
 	fmt.Println("Printing cached convos: ")
+
+	var tempArr []types.ConversationState
 	for i := range proxy.Conversations {
 		if proxy.Conversations[i].ConvoID == "lv5J02gBN3Vvtvdi4JiP" || "7v4i-GgBN3VvtvdiZZzR" == proxy.Conversations[i].ConvoID || "I_5l02gBN3VvtvditZkF" == proxy.Conversations[i].ConvoID || "kf5J02gBN3VvtvdixZgF" == proxy.Conversations[i].ConvoID {
-			fmt.Println("Index" + strconv.Itoa(i) + "needs to be removed")
+			continue
 		}
+		tempArr = append(tempArr, proxy.Conversations[i])
 	}
-	fmt.Println(len(proxy.Conversations))
+
+	err = postChat.UpdateProxyMsg(client.Eclient, id, "Conversations", tempArr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(len(tempArr))
 
 }
