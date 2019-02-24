87ce7aa9109c04db88f7f121f521da5df4b5158f
min <mrb_2059@yahoo.com>
2019-02-24 16:14:28 -0500

    debug

---
+++ admin_main/linkControl.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	getUser "github.com/sea350/ustart_go/get/user"
+	post "github.com/sea350/ustart_go/post/user"
+
+	// post "github.com/sea350/ustart_go/post/user"
+
+	// admin "github.com/sea350/ustart_go/admin"
+
+	"fmt"
+
+	"github.com/sea350/ustart_go/globals"
+	elastic "github.com/olivere/elastic"
+)
+
+var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
+
+func main() {
+
+	usr, err := getUser.UserByUsername(eclient, "support")
+	usrID, err := getUser.IDByUsername(eclient, "support")
+	// usr2, err := getUser.UserByUsername(eclient, "min")
+	usrID2, err := getUser.IDByUsername(eclient, "min")
+
+	fmt.Println(len(usr.QuickLinks))
+	var emp []string{}
+	err = post.UpdateUser(eclient, usrID, "QuickLinks", emp)
+	if err != nil {
+		fmt.Println("LINE 24,", err)
+	}
+
+	err = post.UpdateUser(eclient, usrID2, "QuickLinks", emp)
+	if err != nil {
+		fmt.Println("LINE 24,", err)
+	}
+}
