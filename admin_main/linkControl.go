fb4e61fcaa6c129cdbab2602577dd3faccc87a4d
min <mrb_2059@yahoo.com>
2019-02-25 15:49:07 -0500

    debug

--- admin_main/linkControl.go
+++ admin_main/linkControl.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	// getUser "github.com/sea350/ustart_go/get/user"
-	// post "github.com/sea350/ustart_go/post/user"
+	getUser "github.com/sea350/ustart_go/get/user"
+	post "github.com/sea350/ustart_go/post/user"
 	"github.com/sea350/ustart_go/types"
 
-	getProj "github.com/sea350/ustart_go/get/project"
-	post "github.com/sea350/ustart_go/post/project"
+	// getProj "github.com/sea350/ustart_go/get/project"
+	// post "github.com/sea350/ustart_go/post/project"
 
 	// post "github.com/sea350/ustart_go/post/user"
 
@@ -20,35 +20,35 @@ import (
 
 var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
 
-// func main() {
-
-// 	usr, err := getUser.UserByUsername(eclient, "support")
-// 	usrID, err := getUser.IDByUsername(eclient, "support")
-// 	// usr2, err := getUser.UserByUsername(eclient, "min")
-// 	usrID2, err := getUser.IDByUsername(eclient, "min")
-
-// 	fmt.Println(len(usr.QuickLinks))
-// 	var emp []types.Link
-// 	err = post.UpdateUser(eclient, usrID, "QuickLinks", emp)
-// 	if err != nil {
-// 		fmt.Println("LINE 24,", err)
-// 	}
-
-// 	err = post.UpdateUser(eclient, usrID2, "QuickLinks", emp)
-// 	if err != nil {
-// 		fmt.Println("LINE 24,", err)
-// 	}
-// }
-
 func main() {
 
-	proj, err := getProj.ProjectByURL(eclient, "ITSpackages")
-	projID, err := getProj.ProjectIDByURL(eclient, "ITSpackages")
-	fmt.Println(len(proj.QuickLinks))
+	usr, err := getUser.UserByUsername(eclient, "support")
+	usrID, err := getUser.IDByUsername(eclient, "support")
+	// usr2, err := getUser.UserByUsername(eclient, "min")
+	// usrID2, err := getUser.IDByUsername(eclient, "min")
+
+	fmt.Println(len(usr.QuickLinks))
 	var emp []types.Link
-	err = post.UpdateProject(eclient, projID, "QuickLinks", emp)
+	err = post.UpdateUser(eclient, usrID, "QuickLinks", emp)
 	if err != nil {
-		fmt.Println("LINE 51,", err)
+		fmt.Println("LINE 24,", err)
 	}
 
+	// err = post.UpdateUser(eclient, usrID2, "QuickLinks", emp)
+	// if err != nil {
+	// 	fmt.Println("LINE 24,", err)
+	// }
 }
+
+// func main() {
+
+// 	proj, err := getProj.ProjectByURL(eclient, "ITSpackages")
+// 	projID, err := getProj.ProjectIDByURL(eclient, "ITSpackages")
+// 	fmt.Println(len(proj.QuickLinks))
+// 	var emp []types.Link
+// 	err = post.UpdateProject(eclient, projID, "QuickLinks", emp)
+// 	if err != nil {
+// 		fmt.Println("LINE 51,", err)
+// 	}
+
+// }
