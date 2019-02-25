16f4519ddc12d9502fc631a5db9bd7bf7620335e
min <mrb_2059@yahoo.com>
2019-02-25 16:43:06 -0500

    debug

--- admin_main/allUsers.go
+++ admin_main/allUsers.go
@@ -4,49 +4,48 @@ import (
 
 	// admin "github.com/sea350/ustart_go/admin"
 
-	"context"
-	"encoding/json"
 	"fmt"
 
 	elastic "github.com/olivere/elastic"
+	getUser "github.com/sea350/ustart_go/get/user"
 	"github.com/sea350/ustart_go/globals"
-	"github.com/sea350/ustart_go/types"
 )
 
 var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
 
 //Jv63yWgBN3Vvtvdiu5YP
 
-func main() {
-
-	ctx := context.Background()
-
-	maq := elastic.NewMatchAllQuery()
-	res, err := eclient.Search().
-		Index(globals.UserIndex).
-		Type(globals.UserType).
-		Query(maq).
-		Size(500).
-		Do(ctx)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for _, id := range res.Hits.Hits {
-		data := types.User{}
-		err = json.Unmarshal(*id.Source, &data)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(data.FirstName, "  ", data.LastName, "  ", data.Username)
-	}
-
-}
-
 // func main() {
 
-// 	usr, _ := getUser.UserByID(eclient, "Jv63yWgBN3Vvtvdiu5YP")
-// 	fmt.Println(usr.FirstName, usr.LastName)
+// 	ctx := context.Background()
+
+// 	maq := elastic.NewMatchAllQuery()
+// 	res, err := eclient.Search().
+// 		Index(globals.UserIndex).
+// 		Type(globals.UserType).
+// 		Query(maq).
+// 		Size(500).
+// 		Do(ctx)
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	for _, id := range res.Hits.Hits {
+// 		data := types.User{}
+// 		err = json.Unmarshal(*id.Source, &data)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		fmt.Println(data.FirstName, "  ", data.LastName, "  ", data.Username)
+// 	}
 
 // }
+
+//6v5wyWgBN3Vvtvdiq5Uw
+func main() {
+
+	usr, _ := getUser.UserByID(eclient, "6v5wyWgBN3Vvtvdiq5Uw")
+	fmt.Println(usr.FirstName, usr.LastName)
+
+}
