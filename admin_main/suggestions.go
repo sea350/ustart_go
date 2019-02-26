f16c9c530c83609f4b77a725bb65926ca8f54ad5
min <mrb_2059@yahoo.com>
2019-02-26 17:45:59 -0500

    debug

--- admin_main/suggestions.go
+++ admin_main/suggestions.go
@@ -127,7 +127,7 @@ func sugg(eclient *elastic.Client, class int, tagArray []string, projects []type
 
 func main() {
 
-	minID, err := getUser.IDByUsername(eclient, "ryanrozbiani")
+	minID, err := getUser.IDByUsername(eclient, "yh1112")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -137,7 +137,7 @@ func main() {
 		fmt.Println(err)
 	}
 
-	min, err := getUser.UserByUsername(eclient, "ryanrozbiani")
+	min, err := getUser.UserByUsername(eclient, "yh1112")
 	if err != nil {
 		fmt.Println(err)
 	}
