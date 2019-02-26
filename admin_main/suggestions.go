0b80a488c6b1d0c952e6488c6f139ab9312885fc
min <mrb_2059@yahoo.com>
2019-02-26 16:58:44 -0500

    debug

--- admin_main/suggestions.go
+++ admin_main/suggestions.go
@@ -124,165 +124,194 @@ func sugg(eclient *elastic.Client, class int, tagArray []string, projects []type
 
 }
 
-func main() {
+func main()
+{
 
-	// eclient *elastic.Client, class int, tagArray []string, projects []types.ProjectInfo,
-	// followingUsers map[string]bool, userID string, scrollID string, majors []string, school string
 
 	minID, err := getUser.IDByUsername(eclient, "min")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	ryanID, err := getUser.IDByUsername(eclient, "ryanrozbiani")
-	if err != nil {
-		fmt.Println(err)
-	}
-	stevenID, err := getUser.IDByUsername(eclient, "nevets")
-	if err != nil {
-		fmt.Println(err)
-	}
-	yunjieID, err := getUser.IDByUsername(eclient, "yh1112")
-	if err != nil {
-		fmt.Println(err)
-	}
-	/////////////////////////////////////////////////////////////////////////////////////////////////
-
+		if err != nil {
+			fmt.Println(err)
+		}
+	
 	_, minFoll, err := getFollow.ByID(eclient, minID)
 	if err != nil {
 		fmt.Println(err)
 	}
-	_, ryanFoll, err := getFollow.ByID(eclient, ryanID)
-	if err != nil {
-		fmt.Println(err)
-	}
-	_, stevenFoll, err := getFollow.ByID(eclient, stevenID)
-	if err != nil {
-		fmt.Println(err)
-	}
-	_, yunjieFoll, err := getFollow.ByID(eclient, yunjieID)
-	if err != nil {
-		fmt.Println(err)
-	}
 
-	////////////////////////////////////////////////////////////////////////////////////////////////
 	min, err := getUser.UserByUsername(eclient, "min")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	ryan, err := getUser.UserByUsername(eclient, "ryanrozbiani")
-	if err != nil {
-		fmt.Println(err)
-	}
-	steven, err := getUser.UserByUsername(eclient, "nevets")
-	if err != nil {
-		fmt.Println(err)
-	}
-	yunjie, err := getUser.UserByUsername(eclient, "yh1112")
-	if err != nil {
-		fmt.Println(err)
-	}
-	var minList = []string{}
-	var ryanList = []string{}
-	var stevenList = []string{}
-	var yunjieList = []string{}
 
 	_, h1, _, err := sugg(eclient, min.Class, min.Tags, min.Projects, minFoll.UserFollowing, minID, "", min.Majors, min.University)
-	// s1b, h1b, _, err := sugg(eclient, min.Class, min.Tags, min.Projects, minFoll.UserFollowing, minID, s1a, min.Majors, min.University)
-	// _, h1c, _, err := sugg(eclient, min.Class, min.Tags, min.Projects, minFoll.UserFollowing, minID, s1b, min.Majors, min.University)
-
-	if err != nil && err != io.EOF {
-		fmt.Println(err)
-	}
-
-	if len(h1) > 0 {
-		minList = append(minList, h1[0].FirstName)
-	}
-	// if len(h1b) > 0 {
-	// 	minList = append(minList, h1b[0].FirstName)
-	// }
-	// if len(h1c) > 0 {
-	// 	minList = append(minList, h1c[0].FirstName)
-	// }
-
-	if len(minList) > 0 {
-
-		fmt.Println(minList)
-	}
-
-	_, h2, _, err := sugg(eclient, ryan.Class, ryan.Tags, ryan.Projects, ryanFoll.UserFollowing, ryanID, "", ryan.Majors, ryan.University)
-	// s2b, h2b, _, err := sugg(eclient, ryan.Class, ryan.Tags, ryan.Projects, ryanFoll.UserFollowing, ryanID, s2a, ryan.Majors, ryan.University)
-	// _, h2c, _, err := sugg(eclient, ryan.Class, ryan.Tags, ryan.Projects, ryanFoll.UserFollowing, ryanID, s2b, ryan.Majors, ryan.University)
-
-	if len(h2) > 0 {
-		ryanList = append(minList, h2[0].FirstName)
-		// }
-		// if len(h2b) > 0 {
-		// 	ryanList = append(minList, h2b[0].FirstName)
-		// }
-		// if len(h2c) > 0 {
-		// 	ryanList = append(minList, h2c[0].FirstName)
-		// }
-	}
-	if len(ryanList) != 0 {
-
-		fmt.Println(ryanList)
-	}
-
-	if err != nil && err != io.EOF {
-		fmt.Println(err)
-	}
-
-	_, h3, _, err := sugg(eclient, steven.Class, steven.Tags, steven.Projects, stevenFoll.UserFollowing, stevenID, "", steven.Majors, steven.University)
-	// s3b, h3b, _, err := sugg(eclient, steven.Class, steven.Tags, steven.Projects, stevenFoll.UserFollowing, stevenID, s3a, steven.Majors, steven.University)
-	// _, h3c, _, err := sugg(eclient, steven.Class, steven.Tags, steven.Projects, stevenFoll.UserFollowing, stevenID, s3b, steven.Majors, steven.University)
-
-	if len(h3) > 0 {
-		stevenList = append(minList, h3[0].FirstName)
-	}
-	// if len(h3b) > 0 {
-	// 	stevenList = append(minList, h3b[0].FirstName)
-	// }
-	// if len(h3c) > 0 {
-	// 	stevenList = append(minList, h3c[0].FirstName)
-	// }
-
-	if len(stevenList) != 0 {
-
-		fmt.Println(stevenList)
-	}
-
-	if err != nil && err != io.EOF {
-		fmt.Println(err)
-	}
-
-	_, h4, _, err := sugg(eclient, yunjie.Class, yunjie.Tags, yunjie.Projects, yunjieFoll.UserFollowing, yunjieID, "", yunjie.Majors, yunjie.University)
-	// s4b, h4b, _, err := sugg(eclient, yunjie.Class, yunjie.Tags, yunjie.Projects, yunjieFoll.UserFollowing, yunjieID, s4a, yunjie.Majors, yunjie.University)
-	// _, h4c, _, err := sugg(eclient, yunjie.Class, yunjie.Tags, yunjie.Projects, yunjieFoll.UserFollowing, yunjieID, s4b, yunjie.Majors, yunjie.University)
-
-	// yunjieList = append(yunjieList, h4[0].FirstName, h4b[0].FirstName, h4c[0].FirstName)
-
-	if len(h4) > 0 {
-		stevenList = append(minList, h4[0].FirstName)
-	}
-
-	// if len(h4b) > 0 {
-	// 	stevenList = append(minList, h4b[0].FirstName)
-	// }
-	// if len(h4c) > 0 {
-	// 	stevenList = append(minList, h4c[0].FirstName)
-	// }
-
 	if err != nil {
 		fmt.Println(err)
+	}else{
+		for _, h := range h1{
+			fmt.Println(h.FirstName)
+		}
 	}
-	if len(yunjieList) != 0 && err != io.EOF {
-
-		fmt.Println(yunjieList)
-	}
-
 }
+// func main() {
+
+// 	// eclient *elastic.Client, class int, tagArray []string, projects []types.ProjectInfo,
+// 	// followingUsers map[string]bool, userID string, scrollID string, majors []string, school string
+
+// 	minID, err := getUser.IDByUsername(eclient, "min")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	ryanID, err := getUser.IDByUsername(eclient, "ryanrozbiani")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	stevenID, err := getUser.IDByUsername(eclient, "nevets")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	yunjieID, err := getUser.IDByUsername(eclient, "yh1112")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	/////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 	_, minFoll, err := getFollow.ByID(eclient, minID)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	_, ryanFoll, err := getFollow.ByID(eclient, ryanID)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	_, stevenFoll, err := getFollow.ByID(eclient, stevenID)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	_, yunjieFoll, err := getFollow.ByID(eclient, yunjieID)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	////////////////////////////////////////////////////////////////////////////////////////////////
+// 	min, err := getUser.UserByUsername(eclient, "min")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	ryan, err := getUser.UserByUsername(eclient, "ryanrozbiani")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	steven, err := getUser.UserByUsername(eclient, "nevets")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	yunjie, err := getUser.UserByUsername(eclient, "yh1112")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	var minList = []string{}
+// 	var ryanList = []string{}
+// 	var stevenList = []string{}
+// 	var yunjieList = []string{}
+
+// 	_, h1, _, err := sugg(eclient, min.Class, min.Tags, min.Projects, minFoll.UserFollowing, minID, "", min.Majors, min.University)
+// 	// s1b, h1b, _, err := sugg(eclient, min.Class, min.Tags, min.Projects, minFoll.UserFollowing, minID, s1a, min.Majors, min.University)
+// 	// _, h1c, _, err := sugg(eclient, min.Class, min.Tags, min.Projects, minFoll.UserFollowing, minID, s1b, min.Majors, min.University)
+
+// 	if err != nil && err != io.EOF {
+// 		fmt.Println(err)
+// 	}
+
+// 	if len(h1) > 0 {
+// 		minList = append(minList, h1[0].FirstName)
+// 	}
+// 	// if len(h1b) > 0 {
+// 	// 	minList = append(minList, h1b[0].FirstName)
+// 	// }
+// 	// if len(h1c) > 0 {
+// 	// 	minList = append(minList, h1c[0].FirstName)
+// 	// }
+
+// 	if len(minList) > 0 {
+
+// 		fmt.Println(minList)
+// 	}
+
+// 	_, h2, _, err := sugg(eclient, ryan.Class, ryan.Tags, ryan.Projects, ryanFoll.UserFollowing, ryanID, "", ryan.Majors, ryan.University)
+// 	// s2b, h2b, _, err := sugg(eclient, ryan.Class, ryan.Tags, ryan.Projects, ryanFoll.UserFollowing, ryanID, s2a, ryan.Majors, ryan.University)
+// 	// _, h2c, _, err := sugg(eclient, ryan.Class, ryan.Tags, ryan.Projects, ryanFoll.UserFollowing, ryanID, s2b, ryan.Majors, ryan.University)
+
+// 	if len(h2) > 0 {
+// 		ryanList = append(minList, h2[0].FirstName)
+// 		// }
+// 		// if len(h2b) > 0 {
+// 		// 	ryanList = append(minList, h2b[0].FirstName)
+// 		// }
+// 		// if len(h2c) > 0 {
+// 		// 	ryanList = append(minList, h2c[0].FirstName)
+// 		// }
+// 	}
+// 	if len(ryanList) != 0 {
+
+// 		fmt.Println(ryanList)
+// 	}
+
+// 	if err != nil && err != io.EOF {
+// 		fmt.Println(err)
+// 	}
+
+// 	_, h3, _, err := sugg(eclient, steven.Class, steven.Tags, steven.Projects, stevenFoll.UserFollowing, stevenID, "", steven.Majors, steven.University)
+// 	// s3b, h3b, _, err := sugg(eclient, steven.Class, steven.Tags, steven.Projects, stevenFoll.UserFollowing, stevenID, s3a, steven.Majors, steven.University)
+// 	// _, h3c, _, err := sugg(eclient, steven.Class, steven.Tags, steven.Projects, stevenFoll.UserFollowing, stevenID, s3b, steven.Majors, steven.University)
+
+// 	if len(h3) > 0 {
+// 		stevenList = append(minList, h3[0].FirstName)
+// 	}
+// 	// if len(h3b) > 0 {
+// 	// 	stevenList = append(minList, h3b[0].FirstName)
+// 	// }
+// 	// if len(h3c) > 0 {
+// 	// 	stevenList = append(minList, h3c[0].FirstName)
+// 	// }
+
+// 	if len(stevenList) != 0 {
+
+// 		fmt.Println(stevenList)
+// 	}
+
+// 	if err != nil && err != io.EOF {
+// 		fmt.Println(err)
+// 	}
+
+// 	_, h4, _, err := sugg(eclient, yunjie.Class, yunjie.Tags, yunjie.Projects, yunjieFoll.UserFollowing, yunjieID, "", yunjie.Majors, yunjie.University)
+// 	// s4b, h4b, _, err := sugg(eclient, yunjie.Class, yunjie.Tags, yunjie.Projects, yunjieFoll.UserFollowing, yunjieID, s4a, yunjie.Majors, yunjie.University)
+// 	// _, h4c, _, err := sugg(eclient, yunjie.Class, yunjie.Tags, yunjie.Projects, yunjieFoll.UserFollowing, yunjieID, s4b, yunjie.Majors, yunjie.University)
+
+// 	// yunjieList = append(yunjieList, h4[0].FirstName, h4b[0].FirstName, h4c[0].FirstName)
+
+// 	if len(h4) > 0 {
+// 		stevenList = append(minList, h4[0].FirstName)
+// 	}
+
+// 	// if len(h4b) > 0 {
+// 	// 	stevenList = append(minList, h4b[0].FirstName)
+// 	// }
+// 	// if len(h4c) > 0 {
+// 	// 	stevenList = append(minList, h4c[0].FirstName)
+// 	// }
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	if len(yunjieList) != 0 && err != io.EOF {
+
+// 		fmt.Println(yunjieList)
+// 	}
+
+// }
 
 // eclient *elastic.Client, class int, tagArray []string, projects []types.ProjectInfo,
 // followingUsers map[string]bool, userID string, scrollID string, majors []string, school string
