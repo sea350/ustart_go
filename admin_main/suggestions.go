ecb9b2a3afdabfbb21fe23eaa300da1593f1c153
min <mrb_2059@yahoo.com>
2019-02-26 19:18:41 -0500

    debug

--- admin_main/suggestions.go
+++ admin_main/suggestions.go
@@ -130,7 +130,7 @@ func sugg(eclient *elastic.Client, class int, tagArray []string, projects []type
 }
 
 func main() {
-	unames := []string{"min", "ryanrozbiani", "nevets", "yh1112", "sx563", "julietr", "sc6094", "Carlos"}
+	unames := []string{"min", "ryanrozbiani", "nevets", "yh1112", "sx563", "julietr", "sc6094", "Carlos", "HeatherMT"}
 
 	for _, uname := range unames {
 		usrID, err := getUser.IDByUsername(eclient, uname)
