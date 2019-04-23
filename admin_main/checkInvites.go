6c67e0ed7b9e55d89210656a8b027c6609b1bc6a
min <mrb_2059@yahoo.com>
2019-04-22 20:01:27 -0400

    debug

--- admin_main/checkInvites.go
+++ admin_main/checkInvites.go
@@ -35,7 +35,7 @@ func main() {
 			if err != nil {
 				fmt.Println(err)
 			}
-			fmt.Println(usr.FirstName, usr.LastName)
+			fmt.Println(usr.FirstName, usr.LastName, usr.Username)
 
 		}
 	} else {
