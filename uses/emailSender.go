e64a12c6bbdaee67834c1d9fd42a43313088c93d
mrb721 <mrb_2059@yahoo.com>
2019-08-29 16:10:09 -0400

    debug

--- uses/emailSender.go
+++ uses/emailSender.go
@@ -53,7 +53,7 @@ func (r *Request) parseTemplate(fileName string, data interface{}) error {
 }
 
 func (r *Request) sendMail() bool {
-	body := "To: " + r.to[0] + "\r\nSubject: " + r.subject + "\r\n" + globals.MIME + "\r\n" + r.body
+	// body := "To: " + r.to[0] + "\r\nSubject: " + r.subject + "\r\n" + globals.MIME + "\r\n" + r.body
 
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1")},
