b33810bbeddf182132b0bf5d4c726d79086cb071
mrb721 <mrb_2059@yahoo.com>
2019-09-05 17:08:10 -0400

    term to match q

--- search/prototypeUserSearchScroll.go
+++ search/prototypeUserSearchScroll.go
@@ -76,7 +76,7 @@ func PrototypeUserSearchScroll(eclient *elastic.Client, searchTerm string, sortB
 
 	if len(mustTag) > 0 {
 		for _, tag := range mustTag {
-			query = query.Must(elastic.NewTermQuery("Tags", strings.ToLower(tag)))
+			query = query.Must(elastic.NewMatchQuery("Tags", strings.ToLower(tag)))
 		}
 	}
 
