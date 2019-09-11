acadaf576aa291bc3f8bfc9a345c3d1a0533e369
mrb721 <mrb_2059@yahoo.com>
2019-09-11 13:32:39 -0400

    debug

--- admin_main/recentUsers.go
+++ admin_main/recentUsers.go
@@ -21,7 +21,7 @@ func main() {
 	ctx := context.Background()
 
 	from := time.Date(2019, time.September, 1, 0, 0, 0, 0, time.UTC)
-	maq := elastic.NewBoolQuery().Filter(elastic.NewRangeQuery("AccCreation")).From(from)
+	maq := elastic.NewBoolQuery().Filter(elastic.NewRangeQuery("AccCreation").From(from))
 	res, err := eclient.Search().
 		Index(globals.UserIndex).
 		Type(globals.UserType).
