de78941f1e839ba566ff2fe64287c0f6128e36dc
mrb721 <mrb_2059@yahoo.com>
2019-09-13 11:57:32 -0400

    list coaches

---
+++ admin_main/listCoaches.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+
+	// admin "github.com/sea350/ustart_go/admin"
+
+	"context"
+	"encoding/json"
+	"fmt"
+
+	elastic "github.com/olivere/elastic"
+	"github.com/sea350/ustart_go/globals"
+	"github.com/sea350/ustart_go/types"
+)
+
+var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
+
+func main() {
+
+	ctx := context.Background()
+
+	maq := elastic.NewMatchQuery("Tags", "Coaching")
+	res, err := eclient.Search().
+		Index(globals.UserIndex).
+		Type(globals.UserType).
+		Query(maq).
+		Size(500).
+		// Sort("_score", true).
+		Do(ctx)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, id := range res.Hits.Hits {
+		data := types.User{}
+		err = json.Unmarshal(*id.Source, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(data.FirstName, data.LastName, " ||  ", data.Email)
+	}
+
+}
