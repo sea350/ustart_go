cb98d53a739e232dbe0526b982f0abbde0f4945e
mrb721 <mrb_2059@yahoo.com>
2019-10-04 16:34:20 -0400

    change email

--- admin_main/fixEmail.go
+++ admin_main/fixEmail.go
@@ -10,24 +10,22 @@ import (
 
 	"fmt"
 
-	"github.com/sea350/ustart_go/globals"
 	elastic "github.com/olivere/elastic"
+	"github.com/sea350/ustart_go/globals"
 )
 
 var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
 
 func main() {
 
-	usr, err := getUser.UserByUsername(eclient, "ryanrozbiani")
-	usrID, err := getUser.IDByUsername(eclient, "ryanrozbiani")
+	usr, err := getUser.UserByUsername(eclient, "support")
+	usrID, err := getUser.IDByUsername(eclient, "support")
 
-	fmt.Println(len(usr.Projects))
-	updatesProj := append(usr.Projects[:3], usr.Projects[4:]...)
-	err = post.UpdateUser(eclient, usrID, "Projects", updatesProj)
+	// fmt.Println(len(usr.Projects))
+	// updatesProj := append(usr.Projects[:3], usr.Projects[4:]...)
+	err = post.UpdateUser(eclient, usrID, "Email", "support@ustart.today")
 	if err != nil {
 		fmt.Println("LINE 24,", err)
 	}
-
-	fmt.Println(len(updatesProj))
 
 }
