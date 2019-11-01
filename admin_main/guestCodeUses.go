49638000ecf2b81855b3f6c7597966e2789e8e4b
mrb721 <mrb_2059@yahoo.com>
2019-11-01 17:26:37 -0400

    code check

---
+++ admin_main/guestCodeUses.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+
+	// admin "github.com/sea350/ustart_go/admin"
+
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	elastic "github.com/olivere/elastic"
+	"github.com/sea350/ustart_go/globals"
+	"github.com/sea350/ustart_go/types"
+)
+
+var eclient, _ = elastic.NewSimpleClient(elastic.SetURL(globals.ClientURL))
+
+func main() {
+
+	ctx := context.Background()
+
+	
+	 maq = maq.Must(elastic.NewMatchQuery("FirstName", "joaquin"))
+
+	res, err := eclient.Search().
+		Index(globals.UserIndex).
+		Type(globals.UserType).
+		Query(maq).
+		Size(500).
+		// Sort("_score", true).
+		Do(ctx)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	
+	for _, id := range res.Hits.Hits {
+		data := types.User{}
+		err = json.Unmarshal(*id.Source, &data)
+		fmt.Println(data.Email + "," + data.FirstName + "," + data.LastName, + ',    '+id.Id)
+		guestID := id.Id
+		break
+		if err != nil {
+			fmt.Println(err)
+			
+		}
+	}
+
+
+	maq = maq.Must(elastic.NewMatchQuery("_id", guestID))
+
+	res, err := eclient.Search().
+		Index(globals.UserIndex).
+		Type(globals.UserType).
+		Query(maq).
+		Size(500).
+		// Sort("_score", true).
+		Do(ctx)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, id := range res.Hits.Hits {
+		data := types.GuestCode{}
+		err = json.Unmarshal(*id.Source, &data)
+		fmt.Println(data.Codel + "," + data.Users + "," + data.Expiration)
+		if err != nil {
+			fmt.Println(err)
+			
+		}
+	}
+
+	
+
+
+}
